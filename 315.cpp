479ecf5bd8c7769d8bf2663adef744b769b514f2
skywhat <i@skywhat.com>
2017-06-13 00:15:49 +0800

    315 problem

---
+++ 315.cpp
@@ -0,0 +1,22 @@
+class Solution{
+	public:
+		vector<int> countSmaller(vector<int>& nums){
+			vector<int> res(nums.size(),0);
+			vector<int> fake_bst;
+			for(int i=nums.size()-1;i>=0;i--){
+				int left=0,right=fake_bst.size();
+				while(left<right){
+					int mid=(left+right)/2;
+					if(nums[mid]>fake_bst[mid])
+						mid=left+1;
+					else
+						mid=right;
+				}
+				//not a traditional binary search tree
+				//https://en.wikipedia.org/wiki/Binary_search_algorithm
+				nums[i]=right;
+				fake_bst.insert(fake_bst.begin()+right,nums[i]);
+			}
+			return res;
+		}
+}
