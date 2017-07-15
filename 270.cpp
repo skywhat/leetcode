dc28add2c1b8328106ac171f1fc4bc75a360e7ae
skywhat <i@skywhat.com>
2017-07-15 10:31:19 +0800

    update

--- 270.cpp
+++ 270.cpp
@@ -30,13 +30,17 @@ public:
 
 
 int main() {
-	double target = -1500000000;
 	vector<int> treeListBST = { 1500000000,1400000000 };
+	double target = -1500000000;
+
+	vector<int> treeListBST1 = { 36,13,37,4,20,INT_MIN,48,1,5,17,33,43,49,0,2,INT_MIN,9,14,18,22,34,40,46,INT_MIN,INT_MIN,INT_MIN,INT_MIN,INT_MIN,3,7,11,INT_MIN,16,INT_MIN,19,21,27,INT_MIN,35,39,42,45,47,INT_MIN,INT_MIN,6,8,10,12,15,INT_MIN,INT_MIN,INT_MIN,INT_MIN,INT_MIN,26,32,INT_MIN,INT_MIN,38,INT_MIN,41,INT_MIN,44,INT_MIN,INT_MIN,INT_MIN,INT_MIN,INT_MIN,INT_MIN,INT_MIN,INT_MIN,INT_MIN,INT_MIN,INT_MIN,INT_MIN,INT_MIN,24,INT_MIN,28,INT_MIN,INT_MIN,INT_MIN,INT_MIN,INT_MIN,INT_MIN,INT_MIN,23,25,INT_MIN,29,INT_MIN,INT_MIN,INT_MIN,INT_MIN,INT_MIN,31,30};
+	double target1 = 3.142857;
+
 	Solution s;
 	TreeEnv t;
 
-	TreeNode* root = t.initialTree(treeListBST);
+	TreeNode* root = t.initialTree(treeListBST1);
 	t.displayTree(root);
-	cout << s.closestValue(root, target) << endl;
+	cout << s.closestValue(root, target1) << endl;
 	return 0;
 }
