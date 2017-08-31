166885cecf2b0de839c2e1d8d021c60d4c9f31f6
skywhat <i@skywhat.com>
2017-08-31 00:07:49 -0400

    Remove Duplicates from Sorted Array

---
+++ 26.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution{
+public:
+	int removeDuplicates(vector<int>& nums){
+		if(nums.size()==0)
+			return 0;
+		int start=0;
+		for(int i=0;i<nums.size();++i){
+			int val=nums[start];
+			if(val!=nums[i]){
+				start++;
+				nums[start]=nums[i];
+			}
+		}
+		return start+1;
+	}
+};
+
+int main(){
+	vector<int> testcase={1,1,2};
+	Solution s;
+	int length=s.removeDuplicates(testcase);
+	for(int i=0;i<length;++i){
+		cout<<testcase[i]<<", ";
+	}
+	cout<<endl;
+	return 0;
+}
