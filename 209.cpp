4d307db7e96dad3e36b26b5fae5fff0dcb631de4
skywhat <i@skywhat.com>
2017-09-30 22:25:23 -0400

    Minimum Size Subarray Sum

---
+++ 209.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution{
+public:
+	int minSubArrayLen(int s,vector<int>& nums){
+		if(nums.size()==0)
+			return 0;
+		int i=0,j=0;
+		int minLen=INT_MAX;
+		int sum=0;
+		while(j<nums.size()){
+			sum+=nums[j++];
+			while(sum>=s){
+				minLen=min(minLen,j-i);
+				sum-=nums[i++];
+			}
+		}	
+		return minLen==INT_MAX?0:minLen;
+	}
+};
+
+int main(){
+	vector<int> arr={2,3,1,2,4,3};
+	int len=7;
+	Solution s;
+	cout<<s.minSubArrayLen(len,arr)<<endl;
+
+	return 0;
+}
+
+
