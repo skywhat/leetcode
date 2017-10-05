9b7eac0c72ad48b899fb9595ab8d63d81c94d20e
skywhat <i@skywhat.com>
2017-10-04 23:39:29 -0400

    Meeting Rooms

---
+++ 252.cpp
@@ -0,0 +1,54 @@
+/*
+ Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+ 
+ For example,
+ Given [[0, 30],[5, 10],[15, 20]],
+ return false.
+ 
+
+ */
+
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+struct Interval{
+int start;
+int end;
+Interval():start(0),end(0){}
+Interval(int s,int e):start(s),end(e){}
+};
+
+class Solution{
+public:
+	bool canAttendMeetings(vector<Interval>& intervals){
+		sort(intervals.begin(),intervals.end(),
+				[](Interval i,Interval j){
+					return i.start<j.start;
+				});
+		int i=0;
+		while(i+1<intervals.size()){
+			if(intervals[i].end>intervals[i+1].start)
+				return false;
+			i++;
+		}
+		return true;
+	}
+};
+
+int main(){
+	vector<Interval> test;
+	test.push_back(Interval(0,30));
+	test.push_back(Interval(5,10));
+	test.push_back(Interval(15,20));
+	
+	Solution s;
+	cout<<s.canAttendMeetings(test)<<endl;
+
+	return 0;
+}
+
+
