0ac5eda6d970f53c33323dee03e0a8d61a37f741
skywhat <i@skywhat.com>
2017-10-06 01:22:46 -0400

    First Bad Version

---
+++ 278.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+
+//Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution{
+public:
+	int firstBadVersion(int n){
+		int left=1,right=n;
+		while(left<right){
+			//(low+high)/2 it may cause time limit exceed due to overflow.
+			int mid=left+(right-left)/2;
+			if(isBadVersion(mid))
+				right=mid-1;
+			else
+				left=mid+1;
+		}
+		if(isBadVersion(left))
+			return left;
+		else
+			return left+1;
+	}
+};	
+
+int main(){
+
+}
