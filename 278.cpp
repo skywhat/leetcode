69d06cd7442f850bf36fb4b5eb4b882bc0d96f01
skywhat <i@skywhat.com>
2017-10-14 17:36:49 -0400

    First Bad Version

--- 278.cpp
+++ 278.cpp
@@ -23,6 +23,21 @@ public:
 	}
 };	
 
+class Solution2 {
+public:
+    int firstBadVersion(int n) {
+        int left=1,right=n;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(isBadVersion(mid))
+                right=mid;
+            else
+                left=mid+1;
+        }
+        return left;
+    }
+};
+
 int main(){
 
 }
