d05f16b93af4267f9f14adbc9a41a794a6b4913b
skywhat <i@skywhat.com>
2017-10-22 02:17:40 -0400

    update

--- 88.cpp
+++ 88.cpp
@@ -24,6 +24,24 @@ public:
 	}
 };
 
+class Solution2 {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m-1,j=n-1;
+        int k=m+n-1;
+        while(i>=0&&j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k--]=nums1[i--];
+            }
+            else{
+                nums1[k--]=nums2[j--];
+            }
+        }
+        while(j>=0)
+            nums1[k--]=nums2[j--];
+    }
+};
+
 int main() {
 	vector<int> nums1 = { 0 };
 	int m = 0;
