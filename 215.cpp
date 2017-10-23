28f487d6ac567a4f55ed9511050bc19e19b326c4
skywhat <i@skywhat.com>
2017-10-23 09:20:29 -0400

    priority queue solution

--- 215.cpp
+++ 215.cpp
@@ -58,6 +58,22 @@ public:
 	}
 };
 
+class Solution4 {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>> q;
+        for(int i=0;i<k;++i){
+            q.push(nums[i]);
+        }
+        for(int i=k;i<nums.size();++i){
+            if(q.top()<nums[i]){
+                q.pop();
+                q.push(nums[i]);
+            }
+        }
+        return q.top();
+    }
+};
 
 int main(){
 	vector<int> test={3,2,1,5,6,4};
