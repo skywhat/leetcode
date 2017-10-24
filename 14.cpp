ca51c41574c1eece82c1f871e9e92fe52e55bf70
skywhat <i@skywhat.com>
2017-10-24 00:00:43 -0400

    Longest common prefix

---
+++ 14.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res;
+        if(strs.size()==0)
+            return res;
+        for(int i=0;i<strs[0].size();++i){
+            for(int j=1;j<strs.size();++j){
+                if(strs[j].size()==i)
+                    return res;
+                if(strs[j][i]!=strs[0][i])
+                    return res;
+            }
+            res+=strs[0][i];
+        }
+        return res;
+    }
+};
