940732894910ec0a9874b8372bb29ca27493938e
skywhat <i@skywhat.com>
2017-11-02 17:13:31 -0400

    update

--- 189.cpp
+++ 189.cpp
@@ -22,6 +22,41 @@ public:
 	}		
 };
 
+/*
+ * How to change [0,1,2,3,4,5,6] to [4,5,6,0,1,2,3] by k = 3?
+ *
+ * We can change by the following rule:
+ *
+ *     [0]->[3], [3]->[6], [6]->[2],  [2]->[5], [5]->[1], [1]->[4]
+ */
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        int origin=0;
+        int cur=0;
+        int next=k;
+        // we need two temporary variables to record numbers.
+        int tmp1=nums[cur];
+        int tmp2=nums[next];
+        for(int i=0;i<nums.size();++i){
+            nums[next]=tmp1;//tmp1 is the previous number
+            tmp1=tmp2;
+            cur=next;
+            
+            //if we meet a circle, move to the next one.
+            if(cur==origin){
+                origin=++cur;
+                tmp1=nums[cur];
+            }
+            next=(cur+k)%n;
+            tmp2=nums[next];
+        }
+    }
+};
+
 int main(){
 	vector<int> test={1,2,3,4,5,6,7};
 	int k=3;
