755aa3425f0f5d46918256dde6239f6981eb3b7a
skywhat <i@skywhat.com>
2017-11-07 22:41:31 -0500

    fb interview

---
+++ company/facebook/maxSubtree.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+
+#include "../../Tree.h"
+
+using namespace std;
+
+class Solution{
+public:
+	int maxSubtree(TreeNode* root){
+		if(!root)
+			return 0;
+		if(!root->left&&!root->right)
+			return root->val;
+		int left=maxSubtree(root->left);
+		int right=maxSubtree(root->right);
+		return max(root->val+left+right,max(left,right));	
+	}
+};
+
+/*
+ *     1
+ *    .  .
+ *  2      3
+ *        .  .
+ *     -5     -6
+ *
+ * the max subtree sum is 2
+ *
+ */
+
+int main(){
+	vector<int> l={1,2,3,NULL_TREENODE,NULL_TREENODE,-5,-6};
+	TreeEnv t;
+	TreeNode* root=t.initialTree(l);
+
+	Solution s;
+	cout<<s.maxSubtree(root)<<endl;
+
+	return 0;
+
+}
+
+
+
