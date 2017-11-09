eef1dc24da78a3f11dd0f14a0a1df6c8b74a592a
skywhat <i@skywhat.com>
2017-11-09 16:39:47 -0500

    update

--- 128.cpp
+++ 128.cpp
@@ -3,24 +3,4 @@
 
 using namespace std;
 
-class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
-        int maxLen=0;
-        unordered_set<int> s(nums.begin(),nums.end());
-        for(auto n:s){
-            if(!s.count(n-1)){
-                int cnt=1;
-                while(s.count(n+cnt)){
-                    cnt++;
-                }
-                maxLen=max(maxLen,cnt);
-            }
-        }
-        return maxLen;
-    }
-};
 
-int main(){
-    
-}
--- 191.cpp
+++ 191.cpp
@@ -1,36 +1,15 @@
 #include<iostream>
-#include<vector>
 
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-	int hammingWeight(uint32_t n){
-		int cnt=0;
-		for(int i=0;i<32;++i){
-			if((n>>i)&1)
-				cnt++;
-		}
-		return cnt;
-	}
+    int hammingWeight(uint32_t n) {
+        int cnt=0;
+        while(n){
+            n=n&(n-1);
+            cnt++;
+        }
+        return cnt;
+    }
 };
-
-class Solution1{
-	public:
-		int hammingWeight(uint32_t n){
-			int cnt=0;
-			while(n){
-				cnt++;
-				n&=n-1;
-			}
-			return cnt;
-		}
-};
-
-int main(){
-	unsigned n=11;
-	Solution s;
-	cout<<s.hammingWeight(n)<<endl;
-
-	return 0;
-}
