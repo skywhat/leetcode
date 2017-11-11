9df5df4a00f18fde03da93c72e0b2d0598b54dd6
skywhat <i@skywhat.com>
2017-11-11 11:58:18 -0500

    Plus One

---
+++ 66.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i=digits.size()-1;i>=0;--i){
+            if(digits[i]==9)
+                digits[i]=0;
+            else{
+                digits[i]++;
+                return digits;
+            }
+        }
+        digits[0]=1;
+        digits.push_back(0);
+        return digits;
+    }
+};
+
+int main(){
+
+}
