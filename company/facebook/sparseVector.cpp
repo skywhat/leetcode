3c209fdbb1f4b21c4a11f4661ad4fa81815dc9e4
skywhat <i@skywhat.com>
2017-11-18 18:00:49 -0500

    update

--- company/facebook/sparseVector.cpp
+++ company/facebook/sparseVector.cpp
@@ -30,8 +30,33 @@ public:
 	}
 };
 
+class Solution2{
+public:
+	int dotProduct(vector<pair<int,int>>& v1,vector<pair<int,int>>& v2){
+		sort(v1.begin(),v1.end(),[](pair<int,int> i,pair<int,int> j){
+				return i.first<j.first;
+				});
+		sort(v2.begin(),v2.end(),[](pair<int,int> i,pair<int,int> j){
+				return i.first<j.first;
+				});
+		int i=0,j=0,res=0;
+		while(i<v1.size()&&j<v2.size()){
+			if(v1[i].first<v2[j].first)
+				i++;
+			else if(v1[i].first>v2[j].first)
+				j++;
+			else{
+				res+=v1[i].second*v2[j].second;
+				i++;
+				j++;
+			}
+		}
+		return res;
+	}
+};
+
 int main(){
-	Solution s;
+	Solution2 s;
 	vector<pair<int,int>> v1={
 		{1,4},
 		{4,2},
