558c3e92a8102d7980ed7bab97d6dd654a440581
skywhat <i@skywhat.com>
2017-11-25 00:28:01 -0500

    union find solution

--- 200.cpp
+++ 200.cpp
@@ -32,6 +32,58 @@ public:
     }
 };
 
+//union find
+class Solution2 {
+public:
+    vector<vector<int>> dir={{-1,0},{1,0},{0,-1,},{0,1}};
+    int numIslands(vector<vector<char>>& grid) {
+        int m=grid.size();
+        if(!m)
+            return 0;
+        int cnt=0;
+        int n=grid[0].size();
+        vector<int> root(m*n,0);
+        for(int i=0;i<m;++i){
+            for(int j=0;j<n;++j){
+                root[i*n+j]=i*n+j;
+                if(grid[i][j]=='1')
+                    cnt++;
+            }
+        }
+        for(int i=0;i<m;++i){
+            for(int j=0;j<n;++j){
+                if(grid[i][j]=='0')
+                    continue;
+                int cur=i*n+j;
+                if(root[cur]!=cur)//this position has been unioned.
+                    continue;
+                for(auto&& d:dir){
+                    int x=d[0]+i;
+                    int y=d[1]+j;
+                    if(x<0||x>=m||y<0||y>=n||grid[x][y]=='0')
+                        continue;
+                    int nb=x*n+y;
+                    int rootNb=find(root,nb);
+                    if(find(root,cur)!=rootNb){
+                        root[cur]=rootNb;//union two disjoint sets
+                        cur=rootNb;//update the cur to parent
+                        cnt--;//two islands were unioned.
+                    }
+                }
+
+            }
+        }
+        return cnt;
+    }
+    int find(vector<int>& root,int id){
+        while(root[id]!=id){
+            root[id]=root[root[id]];//path compression
+            id=root[id];
+        }
+        return id;
+    }
+};
+
 /*
  example:
  
