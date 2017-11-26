0eca98d5c8570aa704d49e967d8856eb1e90a7f1
skywhat <i@skywhat.com>
2017-11-26 00:30:39 -0500

    update union find path compression

--- 261.cpp
+++ 261.cpp
@@ -25,5 +25,32 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    bool validTree(int n, vector<pair<int, int>>& edges) {
+        vector<int> uf(n,0);
+        for(int i=0;i<n;++i)
+            uf[i]=i;
+        for(auto&& e:edges){
+            int l=find(uf,e.first);
+            int r=find(uf,e.second);
+            if(l!=r){
+                uf[l]=r;
+            }
+            else
+                return false;
+        }
+        return edges.size()==n-1;
+        
+    }
+    int find(vector<int>& uf,int id){
+        while(uf[id]!=id){
+            uf[id]=uf[uf[id]];//path compression.
+            id=uf[id];
+        }
+        return id;
+    }
+};
+
 int main(){
 }
