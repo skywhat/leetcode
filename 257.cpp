7db6a957b0d5ce160b87ba3202a5b44a94418c0d
skywhat <i@skywhat.com>
2017-11-26 00:06:49 -0500

    iterative solution

--- 257.cpp
+++ 257.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+//recursive
 class Solution {
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
@@ -22,6 +23,40 @@ public:
                 dfs(root->right,res,temp+"->");
             }
         }
+    }
+};
+
+//iterative
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        queue<TreeNode*> q;
+        queue<string> paths;
+        vector<string> res;
+        if(root){
+            q.push(root);
+            paths.push(to_string(root->val));
+        }
+        while(!q.empty()){
+            int size=q.size();
+            while(size--){
+                TreeNode* cur=q.front();q.pop();
+                string curP=paths.front();paths.pop();
+                if(!cur->left&&!cur->right){
+                    res.push_back(curP);
+                    continue;
+                }
+                if(cur->left){
+                    q.push(cur->left);
+                    paths.push(curP+"->"+to_string(cur->left->val));
+                }
+                if(cur->right){
+                    q.push(cur->right);
+                    paths.push(curP+"->"+to_string(cur->right->val));
+                }
+            }
+        }
+        return res;
     }
 };
 
