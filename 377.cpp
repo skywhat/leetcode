f18db159d5fe0afb6938a573792a2dd4a282b468
skywhat <i@skywhat.com>
2017-11-27 00:43:57 -0500

    update

--- 377.cpp
+++ 377.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//Time Limit Exceeded.
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
@@ -15,6 +16,23 @@ public:
             res+=combinationSum4(nums,target-nums[i]);
         }
         return res;
+    }
+};
+
+//dynamic programming
+class Solution2 {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1,0);
+        dp[0]=1;
+        for(int i=1;i<=target;++i){
+            for(int j=0;j<nums.size();++j){
+                if(i-nums[j]>=0){
+                    dp[i]+=dp[i-nums[j]];
+                }
+            }
+        }
+        return dp[target];
     }
 };
 
