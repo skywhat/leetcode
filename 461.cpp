454c2a7e12b1ee61d10f030e27f920a2880caa41
skywhat <i@skywhat.com>
2018-08-01 13:58:14 -0700

    Update 461.cpp
    cleaner code, better naming.

--- 461.cpp
+++ 461.cpp
@@ -5,14 +5,16 @@ using namespace std;
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        int temp=x^y;
-        int sum=0;
-        for(int i=0;i<=31;++i){
-            if(temp&1)
-            sum++;
-            temp>>=1;
+        int x_xor_y = x^y;
+        int cnt = 0;
+        
+        for(int i=0;i<32;++i){
+            if(1&(x_xor_y >> i)){
+                cnt++;
+            }
         }
-        return sum;
+        
+        return cnt;
     }
 };
 
