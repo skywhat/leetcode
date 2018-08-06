4316b383b2c11502a0ca2a68a9fada75c6d73e72
skywhat <i@skywhat.com>
2018-08-05 21:56:09 -0700

    Update 226.cpp
    cleaner code swapping tree node

--- 226.cpp
+++ 226.cpp
@@ -17,6 +17,20 @@ public:
 		return root;
 	}
 };
+
+class Solution2 {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root){
+            invertTree(root->left);
+            invertTree(root->right);
+            swap(root->left, root->right);
+        }
+        
+        return root;
+    }
+};
+
 int main() {
 	TreeEnv t;
 	vector<int> list = {4,2,7,1,3,6,9};
