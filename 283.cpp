fc6b5b06cfdd7e02ae2cd6ee6555132f639e7a83
skywhat <i@skywhat.com>
2018-08-06 22:55:49 -0400

    Update 283.cpp
    move non-zero by swapping the zero on the left

--- 283.cpp
+++ 283.cpp
@@ -36,6 +36,27 @@ public:
 	}
 };
 
+class Solution3 {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int begin = 0;
+        for(int i=0;i<nums.size();++i){
+            if(nums[i]!=0){
+                swap(nums[i],nums[begin++]);
+            }
+        }
+    }
+};
+
+/*
+0,1,0,3,12
+
+1,0,0,3,12
+1,3,0,0,12
+1,3,12,0,0
+
+*/
+
 int main(){
 	vector<int> test={0,1,0,3,12};
 	Solution s;
