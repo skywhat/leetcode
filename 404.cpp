27d41fa683f19ed6e313300ab4f77170942f1b52
skywhat <i@skywhat.com>
2018-08-08 19:08:05 -0400

    Update 404.cpp
    update, cleaner code

--- 404.cpp
+++ 404.cpp
@@ -9,41 +9,47 @@ using namespace std;
 class Solution {
 public:
     int sumOfLeftLeaves(TreeNode* root) {
-        int res=0;
         stack<TreeNode*> s;
-        if(root)
+        int sum_of_left_leaves = 0;
+        if(root){
             s.push(root);
+        }
+        
         while(!s.empty()){
-            TreeNode* cur=s.top();
+            TreeNode* cur = s.top();
             s.pop();
+            
             if(cur->left){
-                if(!cur->left->left&&!cur->left->right)
-                    res+=cur->left->val;
-                else
+                if(!cur->left->left && !cur->left->right){
+                    sum_of_left_leaves += cur->left->val;
+                }
+                else {
                     s.push(cur->left);
+                }
             }
+            
             if(cur->right){
-                if(cur->right->left||cur->right->right)
-                    s.push(cur->right);
+                s.push(cur->right);
             }
         }
-        return res;
+        
+        return sum_of_left_leaves;
     }
 };
 
 //recursive
-class Solution {
+class Solution2 {
 public:
     int sumOfLeftLeaves(TreeNode* root) {
-        if(!root)
+        if(!root){
             return 0;
-        if(root->left){
-            if(!root->left->left&&!root->left->right)
-                return root->left->val+sumOfLeftLeaves(root->right);
-            return sumOfLeftLeaves(root->left)+sumOfLeftLeaves(root->right);
         }
-        return sumOfLeftLeaves(root->right);
         
+        if(root->left && !root->left->left && !root->left->right){
+            return root->left->val + sumOfLeftLeaves(root->right);
+        }
+        
+        return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
     }
 };
 
