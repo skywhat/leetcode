6cf04811a01e3d237d7ef5378b547160d2ed4992
skywhat <i@skywhat.com>
2018-08-10 17:15:40 -0400

    Update 191.cpp

--- 191.cpp
+++ 191.cpp
@@ -5,9 +5,9 @@ using namespace std;
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int cnt=0;
+        int cnt = 0;
         while(n){
-            n=n&(n-1);
+            n = n&(n-1);
             cnt++;
         }
         return cnt;
