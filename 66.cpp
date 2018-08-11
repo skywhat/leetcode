8d1a29084bf24efa029a60a9577e0f6f53ab832b
skywhat <i@skywhat.com>
2018-08-11 12:32:59 -0400

    Update 66.cpp

--- 66.cpp
+++ 66.cpp
@@ -7,14 +7,16 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         for(int i=digits.size()-1;i>=0;--i){
-            if(digits[i]==9)
-                digits[i]=0;
+            if(digits[i] == 9){
+                digits[i] = 0;
+            }
             else{
                 digits[i]++;
                 return digits;
             }
         }
-        digits[0]=1;
+        
+        digits[0] = 1;
         digits.push_back(0);
         return digits;
     }
