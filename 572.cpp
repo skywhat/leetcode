76bcb15ad02be77ce6bc9e8c7bd9192766e671bc
skywhat <i@skywhat.com>
2018-08-11 01:09:18 -0400

    Update 572.cpp

--- 572.cpp
+++ 572.cpp
@@ -1,24 +1,30 @@
 #include<iostream>
 #include "Tree.h"
 
-
 class Solution {
 public:
     bool isSubtree(TreeNode* s, TreeNode* t) {
-        if(s==nullptr)
-        return false;
-        return isSametree(s,t)||isSubtree(s->left,t)||isSubtree(s->right,t);
-    }
-    bool isSametree(TreeNode* s,TreeNode* t){
-        if(s==nullptr&&t==nullptr)
-        return true;
-        if(s==nullptr||t==nullptr)
-        return false;
-        if(s->val==t->val){
-            return isSametree(s->left,t->left)&&isSametree(s->right,t->right);
+        if(!s){
+            return false;
         }
-        else
-        return false;
         
+        return isSameTree(s, t) || isSubtree(s->left, t) || isSubtree(s->right, t); 
+    }
+    
+    bool isSameTree(TreeNode* s, TreeNode* t){
+        if(!s && !t){
+            return true;
+        }
+        
+        if(!s || !t){
+            return false;
+        }
+        
+        if(s->val != t->val){
+            return false;
+        }
+        
+        return isSameTree(s->left, t->left) && 
+            isSameTree(s->right, t->right);
     }
 };
