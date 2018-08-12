0093dd0ab002b981bf3698c179b7894633bd2817
skywhat <i@skywhat.com>
2018-08-12 16:26:46 -0400

    Update 110.cpp

--- 110.cpp
+++ 110.cpp
@@ -3,28 +3,33 @@
 
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-	bool isBalanced(TreeNode* root){
-		if(getHeight(root)==-1)
-			return false;
-		return true;
-	}
-
-	int getHeight(TreeNode* root){
-		if(!root)
-			return 0;
-		int left=getHeight(root->left);
-		if(left==-1)
-			return -1;
-		int right=getHeight(root->right);
-		if(right==-1)
-			return -1;
-		if(abs(left-right)>1)
-			return -1;
-		return max(left,right)+1;
-	}
-
+    bool isBalanced(TreeNode* root) {
+        return getHeight(root) != -1;
+    }
+    
+    int getHeight(TreeNode* root){
+        if(!root){
+            return 0;
+        }
+        
+        int left = getHeight(root->left);
+        if(left == -1){
+            return -1;
+        }
+        
+        int right = getHeight(root->right);
+        if(right == -1){
+            return -1;
+        }
+        
+        if(abs(left - right)>1){
+            return -1;
+        }
+        
+        return max(left, right)+1;
+    }
 };
 //in this bottom up approach, each node in the tree only need to be accessed once. Thus the time complexity is O(n)
 int main(){
