60c81abd088d4cebf952cce055beb7f6d921bc9d
skywhat <i@skywhat.com>
2018-08-13 01:11:43 -0400

    Update 234.cpp

--- 234.cpp
+++ 234.cpp
@@ -6,35 +6,40 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if(!head||!head->next)
+        if(!head || !head->next){
             return true;
-        ListNode* slow=head,*fast=head;
-        while(fast->next&&fast->next->next){
-            fast=fast->next->next;
-            slow=slow->next;
         }
-        slow->next=reverse(slow->next);
-        slow=slow->next;
+        
+        ListNode* slow = head, *fast = head;
+        while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        slow->next = reverse(slow->next);
+        slow = slow->next;
         while(slow){
-            if(head->val!=slow->val)
+            if(head->val != slow->val){
                 return false;
-            head=head->next;
-            slow=slow->next;
+            }
+            head = head->next;
+            slow = slow->next;
         }
+        
         return true;
-
     }
+    
     ListNode* reverse(ListNode* head){
-        ListNode* prev=nullptr;
-        ListNode* cur;
+        ListNode* prev = nullptr;
+        ListNode* temp = head;
         while(head){
-            cur=head->next;
-            head->next=prev;
-            prev=head;
-            head=cur;
+            temp = head->next;
+            head->next = prev;
+            prev = head;
+            head = temp;
         }
         return prev;
-    }
+    } 
 };
 /*
 1-2-3-4-5
