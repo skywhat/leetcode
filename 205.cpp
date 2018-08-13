f2a9cd955e9135ff47911a6a6a66bafedfffd109
skywhat <i@skywhat.com>
2018-08-12 22:43:53 -0400

    Update 205.cpp

--- 205.cpp
+++ 205.cpp
@@ -26,16 +26,20 @@ public:
 class Solution2 {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s.size()!=t.size())
+        if(s.size() != t.size()){
             return false;
-        vector<int> m1(128,0);
-        vector<int> m2(128,0);
-        for(int i=0;i<s.size();++i){
-            if(m1[s[i]]!=m2[t[i]])
-                return false;
-            m1[s[i]]=i+1;
-            m2[t[i]]=i+1;
         }
+        
+        vector<int> m1(128, 0);
+        vector<int> m2(128, 0);
+        for(int i=0;i<s.size();++i){
+            if(m1[s[i]] != m2[t[i]]){
+                return false;
+            }
+            m1[s[i]] = i+1;
+            m2[t[i]] = i+1;
+        }
+        
         return true;
     }
 };
