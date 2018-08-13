fe3b7be92521540398c54a183c91a92e57bf4b7a
skywhat <i@skywhat.com>
2018-08-12 23:28:52 -0400

    Update 20.cpp

--- 20.cpp
+++ 20.cpp
@@ -4,44 +4,40 @@
 #include<stack>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-	bool isValid(string s){
-		stack<char> bt;
-		for(int i=0;i<s.size();++i){
-			if(s[i]=='(')
-				bt.push(s[i]);
-			else if(s[i]=='{')
-				bt.push(s[i]);
-			else if(s[i]=='[')
-				bt.push(s[i]);
-			else if(s[i]==')'){
-				if(bt.top()!='(')
-					return false;
-				else
-					bt.pop();
-			}
-			else if(s[i]=='}'){
-				if(bt.top()!='{')
-					return false;
-				else
-					bt.pop();
-			}
-			else if(s[i]==']'){
-				if(bt.top()!='[')
-					return false;
-				else 
-					bt.pop();
-			}
-		}	
-		if(bt.empty())
-			return true;
-		else
-			return false;
-	}
+    bool isValid(string s) {
+        stack<char> st;
+        int i=0;
+        while(i<s.size()){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+                st.push(s[i]);
+            }
+            else if(s[i] == ')'){
+                if(st.empty() || st.top() != '('){
+                    return false;
+                }
+                st.pop();
+            }
+            else if(s[i] == ']'){
+                if(st.empty() || st.top() != '['){
+                    return false;
+                }
+                st.pop();
+            }
+            else if(s[i] == '}'){
+                if(st.empty() || st.top() != '{'){
+                    return false;
+                }
+                st.pop();
+            }
+            i++;
+        }
+        
+        return st.empty();
+    }
 };
 
 int main(){
-
 
 }
