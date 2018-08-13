6f66da9bb72c4078070d479d7336f3b586cbbbfa
skywhat <i@skywhat.com>
2018-08-13 15:40:00 -0400

    Update 58.cpp

--- 58.cpp
+++ 58.cpp
@@ -6,16 +6,17 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        if(s.size()==0)
-            return 0;
-        int cnt=0;
-        int i=s.size()-1;
-        while(i>=0&&s[i]==' ')
+        int i = s.size()-1;
+        int cnt = 0;
+        
+        while(i>=0 && s[i]==' '){
             i--;
-        while(i>=0&&s[i]!=' '){
+        }
+        while(i>=0 && s[i]!=' '){
             cnt++;
             i--;
         }
+        
         return cnt;
     }
 };
