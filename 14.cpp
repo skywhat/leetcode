ac28aaf3d0afcafd91c621e15d6ddbcb5174375b
skywhat <i@skywhat.com>
2018-08-13 15:51:40 -0400

    Update 14.cpp

--- 14.cpp
+++ 14.cpp
@@ -6,18 +6,20 @@ using namespace std;
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size() == 0){
+            return "";
+        }
+        
         string res;
-        if(strs.size()==0)
-            return res;
         for(int i=0;i<strs[0].size();++i){
             for(int j=1;j<strs.size();++j){
-                if(strs[j].size()==i)
+                if(strs[j].size()<=i || strs[j][i] != strs[0][i]){
                     return res;
-                if(strs[j][i]!=strs[0][i])
-                    return res;
+                }
             }
-            res+=strs[0][i];
+            res += strs[0][i];
         }
+        
         return res;
     }
 };
