8865c42146d77fca9cf13460816eb34ea72f6e07
skywhat <i@skywhat.com>
2018-08-13 20:54:02 -0400

    Update 278.cpp

--- 278.cpp
+++ 278.cpp
@@ -1,40 +1,24 @@
 #include<iostream>
 #include<vector>
 
-//Forward declaration of isBadVersion API.
+// Forward declaration of isBadVersion API.
 bool isBadVersion(int version);
 
-class Solution{
-public:
-	int firstBadVersion(int n){
-		int left=1,right=n;
-		while(left<right){
-			//(low+high)/2 it may cause time limit exceed due to overflow.
-			int mid=left+(right-left)/2;
-			if(isBadVersion(mid))
-				right=mid-1;
-			else
-				left=mid+1;
-		}
-		if(isBadVersion(left))
-			return left;
-		else
-			return left+1;
-	}
-};	
-
-class Solution2 {
+class Solution {
 public:
     int firstBadVersion(int n) {
-        int left=1,right=n;
-        while(left<right){
-            int mid=left+(right-left)/2;
-            if(isBadVersion(mid))
-                right=mid;
-            else
-                left=mid+1;
+        int i=1, j=n;
+        while(i+1 < j){
+            int mid = i+(j-i)/2;
+            if(isBadVersion(mid)){
+                j=mid;
+            }
+            else{
+                i=mid;
+            }
         }
-        return left;
+        
+        return isBadVersion(i)? i:j;
     }
 };
 
