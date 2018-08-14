191b602d268e11a7e99caf272b10225ad6c9f305
skywhat <i@skywhat.com>
2018-08-13 20:43:40 -0400

    Update 125.cpp

--- 125.cpp
+++ 125.cpp
@@ -4,31 +4,40 @@
 
 using namespace std;
 
-class Solution{
+class Solution {
+private:
+        vector<int> alpha_numeric;
 public:
-	bool isPalindrome(string s){
-		vector<int> m(256,0);
-		for(int i='0';i<='9';++i)
-			m[i]=i;
-		for(int i='a';i<='z';++i)
-			m[i]=i;
-		for(int i='A';i<='Z';++i)
-			m[i]=i-('A'-'a');
-		int i=0,j=s.size()-1;
-		while(i<j){
-			while(m[s[i]]==0)
-				i++;
-			while(m[s[j]]==0)
-				j--;
-			if(i>=j)
-				break;
-			if(m[s[i]]!=m[s[j]])
-				return false;
-			i++;
-			j--;
-		}
-		return true;
-	}
+    Solution(){
+        alpha_numeric.assign(128, 0);
+        for(int i='a';i<='z';++i){
+            alpha_numeric[i] = i;
+        }
+        for(int i='A';i<='Z';++i){
+            alpha_numeric[i] = i-'A'+'a';
+        }
+        for(int i='0';i<='9';++i){
+            alpha_numeric[i] = i;
+        }
+    }
+    bool isPalindrome(string s) {
+        int i=0,j=s.size()-1;
+        while(i<j){
+            if(alpha_numeric[s[i]] == 0){
+                i++;
+                continue;
+            }
+            if(alpha_numeric[s[j]] == 0){
+                j--;
+                continue;
+            }
+            if(alpha_numeric[s[i++]] != alpha_numeric[s[j--]]){
+                return false;
+            }
+        }
+        
+        return true;
+    }
 };
 
 int main(){
