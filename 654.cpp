c0bc071e70ea1f2456118b0660683a56edfbfae8
skywhat <i@skywhat.com>
2018-08-15 12:52:04 -0400

    remove else

--- 654.cpp
+++ 654.cpp
@@ -13,23 +13,23 @@ public:
     TreeNode* constructMaximumBinaryTree(vector<int>& nums, int left, int right) {
         if (left > right) {
             return nullptr;
-        } else {
-            int root_index = left;
-            int max_value = nums[left];
-
-            for (int i = left + 1; i <= right; ++i) {
-                if (nums[i] > max_value) {
-                    max_value = nums[i];
-                    root_index = i;
-                }
-            }
-
-            TreeNode* root = new TreeNode(max_value);
-            root->left = constructMaximumBinaryTree(nums, left, root_index - 1);
-            root->right = constructMaximumBinaryTree(nums, root_index + 1, right);
-
-            return root;
         }
+
+        int root_index = left;
+        int max_value = nums[left];
+
+        for (int i = left + 1; i <= right; ++i) {
+            if (nums[i] > max_value) {
+                max_value = nums[i];
+                root_index = i;
+            }
+        }
+
+        TreeNode* root = new TreeNode(max_value);
+        root->left = constructMaximumBinaryTree(nums, left, root_index - 1);
+        root->right = constructMaximumBinaryTree(nums, root_index + 1, right);
+
+        return root;
     }
 };
 
