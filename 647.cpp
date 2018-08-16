df97a47090d1d73512c067bf1dbfc5d20e7fe72e
skywhat <i@skywhat.com>
2018-08-16 01:59:13 -0400

    Palindromic Substrings

--- 647.cpp
+++ 647.cpp
@@ -1,34 +1,52 @@
-#include<iostream>
-#include<string>
+/*
+
+Given a string, your task is to count how many palindromic substrings in this
+string.
+
+The substrings with different start indexes or end indexes are counted as
+different substrings even they consist of same characters.
+
+Example 1:
+Input: "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+Input: "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+Note:
+The input string length won't exceed 1000.
+*/
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-	int countSubstrings(string s){
-		int count=0;
-		for(int i=0;i<s.size();++i){
-			extend(s,i,i,count);//odd string
-			extend(s,i,i+1,count);//even string
-		}
-		return count;
-	}
-	void extend(const string& s,int left,int right,int& count){
-		while(left>=0&&right<s.size()&&s[left]==s[right]){
-			count++;
-			left--;
-			right++;
-		}
-	}
+    int countSubstrings(string s) {
+        int count = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            extendPalindromic(s, i, i, count);
+            extendPalindromic(s, i, i + 1, count);
+        }
+
+        return count;
+    }
+
+    void extendPalindromic(const string& s, int i, int j, int& count) {
+        while (i >= 0 && j < s.size() && s[i] == s[j]) {
+            count++;
+            i--;
+            j++;
+        }
+    }
 };
 
-int main(){
-	string test="abc";
-	Solution s;
-	cout<<s.countSubstrings(test)<<endl;
+int main() {
+    string test = "abc";
+    Solution s;
+    cout << s.countSubstrings(test) << endl;
 
-	return 0;
+    return 0;
 }
-
-
-
