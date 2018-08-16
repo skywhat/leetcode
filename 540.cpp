5e682b78a2c56553726172e5c4e4978f710ef009
skywhat <i@skywhat.com>
2018-08-15 23:56:32 -0400

    update

--- 540.cpp
+++ 540.cpp
@@ -1,29 +1,32 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-	int singleNonDuplicate(vector<int>& nums){
-		int low=0,high=nums.size()/2,mid;
-		while(low<high){
-			mid=(low+high)/2;
-			if(nums[2*mid]==nums[2*mid+1])
-				low=mid+1;
-			else
-				high=mid;
-		}
-		return nums[2*low];
-	}
+    int singleNonDuplicate(vector<int>& nums) {
+        int low = 0, high = nums.size() / 2;
+        while (low + 1 < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[2 * mid] == nums[2 * mid + 1]) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+
+        return nums[2 * low] == nums[2 * low + 1] ? nums[2 * high]
+                                                  : nums[2 * low];
+    }
 };
 
-int main(){
-	vector<int> testcase={1,1,2,3,3,4,4,8,8};
-	vector<int> testcase1={3,3,7,7,10,11,11};
+int main() {
+    vector<int> testcase = {1, 1, 2, 3, 3, 4, 4, 8, 8};
+    vector<int> testcase1 = {3, 3, 7, 7, 10, 11, 11};
 
-	Solution s;
-	cout<<s.singleNonDuplicate(testcase1)<<endl;
+    Solution s;
+    cout << s.singleNonDuplicate(testcase1) << endl;
 
-	return 0;
+    return 0;
 }
