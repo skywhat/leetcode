84ab60c5a406cc85227c11498ed6c8db93915950
skywhat <i@skywhat.com>
2018-08-17 00:31:22 -0400

    Binary Tree Inorder Traversal

--- 94.cpp
+++ 94.cpp
@@ -1,23 +1,22 @@
-#include<iostream>
-#include<vector>
-#include<stack>
 #include "Tree.h"
+#include <iostream>
+#include <stack>
+#include <vector>
 
 using namespace std;
-
 
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res;
-        inorder(root,res);
+        inorder(root, res);
         return res;
     }
-    void inorder(TreeNode* root,vector<int>& res){
-        if(root){
-            inorder(root->left,res);
+    void inorder(TreeNode* root, vector<int>& res) {
+        if (root) {
+            inorder(root->left, res);
             res.push_back(root->val);
-            inorder(root->right,res);
+            inorder(root->right, res);
         }
     }
 };
@@ -25,20 +24,20 @@ public:
 class Solution2 {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> res;
         stack<TreeNode*> s;
-        TreeNode* curNode=root;
-        while(curNode||!s.empty()){
-            if(curNode){
-                s.push(curNode);
-                curNode=curNode->left;
-            }else{
-                curNode=s.top();
-                s.pop();
-                res.push_back(curNode->val);
-                curNode=curNode->right;
+        vector<int> res;
+
+        while (root || !s.empty()) {
+            while (root) {
+                s.push(root);
+                root = root->left;
             }
+            root = s.top();
+            s.pop();
+            res.push_back(root->val);
+            root = root->right;
         }
+
         return res;
     }
 };
