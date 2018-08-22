947e71dd031e6f8cb1ee5fa37bcf69120cf623df
skywhat <i@skywhat.com>
2018-08-21 23:57:40 -0400

    623. Add One Row to Tree

--- 623.cpp
+++ 623.cpp
@@ -1,61 +1,74 @@
-#include<iostream>
-#include<vector>
-#include<queue>
 #include "Tree.h"
+#include <iostream>
+#include <queue>
+#include <vector>
 using namespace std;
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
-	TreeNode* addOneRow(TreeNode* root, int v, int d) {
-		if (d == 1) {
-			TreeNode* newRoot = new TreeNode(v);
-			newRoot->left = root;
-			return newRoot;
-		}
-		queue<TreeNode*> q;
-		int depth = 2;
-		if (root)
-			q.push(root);
-		while (!q.empty()) {
-			int size = q.size();
-			while (size--) {
-				TreeNode* tmp = q.front();
-				q.pop();
-				if (d == depth) {
-					TreeNode* new_left = new TreeNode(v);
-					TreeNode* new_right = new TreeNode(v);
-					new_left->left = tmp->left;
-					new_right->right = tmp->right;
-					tmp->left = new_left;
-					tmp->right = new_right;
+    TreeNode* addOneRow(TreeNode* root, int v, int d) {
+        if (d == 1) {
+            TreeNode* new_root = new TreeNode(v);
+            new_root->left = root;
+            return new_root;
+        }
 
-				}
-				else {
-					if (tmp->left)
-						q.push(tmp->left);
-					if (tmp->right)
-						q.push(tmp->right);
-				}
-			}
-			depth++;
-			if (d < depth)
-				break;
-		}
-		return root;
-	}
+        queue<TreeNode*> nodes;
+        if (root) {
+            nodes.push(root);
+        }
+
+        int depth = 2;
+        while (!nodes.empty()) {
+            int size = nodes.size();
+            while (size--) {
+                TreeNode* temp = nodes.front();
+                nodes.pop();
+                if (depth == d) {
+                    TreeNode* new_left = new TreeNode(v);
+                    TreeNode* new_right = new TreeNode(v);
+                    new_left->left = temp->left;
+                    new_right->right = temp->right;
+                    temp->left = new_left;
+                    temp->right = new_right;
+                } else {
+                    if (temp->left) {
+                        nodes.push(temp->left);
+                    }
+                    if (temp->right) {
+                        nodes.push(temp->right);
+                    }
+                }
+            }
+            depth++;
+            if (depth > d) {
+                break;
+            }
+        }
+
+        return root;
+    }
 };
 
 int main() {
-	TreeEnv t;
-	vector<int> list = { 4,2,6,3,1,5 };
-	TreeNode* root = t.initialTree(list);
+    TreeEnv t;
+    vector<int> list = {4, 2, 6, 3, 1, 5};
+    TreeNode* root = t.initialTree(list);
 
-	Solution s;
+    Solution s;
 
-	t.displayTree(root);
-	TreeNode* newOne = s.addOneRow(root, 1, 4);
-	t.displayTree(newOne);
+    t.displayTree(root);
+    TreeNode* newOne = s.addOneRow(root, 1, 4);
+    t.displayTree(newOne);
 
-	return 0;
-
+    return 0;
 }
