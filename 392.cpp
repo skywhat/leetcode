47f36d097546eddb7aee6bb5eb9605aeff6107d4
skywhat <i@skywhat.com>
2018-08-23 00:38:03 -0400

    392. Is Subsequence

--- 392.cpp
+++ 392.cpp
@@ -1,29 +1,55 @@
-#include<iostream>
-#include<vector>
-#include<string>
+/*
+Given a string s and a string t, check if s is subsequence of t.
+
+You may assume that there is only lower case English letters in both s and t. t
+is potentially a very long (length ~= 500,000) string, and s is a short string
+(<=100).
+
+A subsequence of a string is a new string which is formed from the original
+string by deleting some (can be none) of the characters without disturbing the
+relative positions of the remaining characters. (ie, "ace" is a subsequence of
+"abcde" while "aec" is not).
+
+Example 1:
+s = "abc", t = "ahbgdc"
+
+Return true.
+
+Example 2:
+s = "axc", t = "ahbgdc"
+
+Return false.
+
+Follow up:
+If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you
+want to check one by one to see if T has its subsequence. In this scenario, how
+would you change your code?
+*/
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-	bool isSubsequence(string s, string t) {
-		int i, j;
-		for (i = 0, j = 0; i < t.size() && j < s.size(); ++i) {
-			if (s[j] == t[i])
-				j++;
-		}
-		if (j >= s.size())
-			return true;
-		else
-			return false;
-	}
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+        while (i < s.size() && j < t.size()) {
+            if (s[i] == t[j++]) {
+                i++;
+            }
+        }
+
+        return i >= s.size();
+    }
 };
 
 int main() {
-	string s = "abc";
-	string t = "ahbgdc";
-	Solution solution;
-	cout << solution.isSubsequence(s, t) << endl;
+    string s = "abc";
+    string t = "ahbgdc";
+    Solution solution;
+    cout << solution.isSubsequence(s, t) << endl;
 
-	return 0;
+    return 0;
 }
