96a9ce3fde05fbe98fb2de9bed69e587a21bec25
skywhat <i@skywhat.com>
2018-08-23 23:54:00 -0400

    261. Graph Valid Tree

--- 261.cpp
+++ 261.cpp
@@ -1,56 +1,62 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     bool validTree(int n, vector<pair<int, int>>& edges) {
-        vector<int> uf(n,-1);
-        for(int i=0;i<edges.size();++i){
-            int x=find(uf,edges[i].first);
-            int y=find(uf,edges[i].second);
-            if(x==y)
+        vector<int> uf(n, -1);
+
+        for (int i = 0; i < edges.size(); ++i) {
+            int v1 = find(uf, edges[i].first);
+            int v2 = find(uf, edges[i].second);
+            if (v1 == v2) {
                 return false;
-            uf[x]=y;
+            }
+
+            uf[v1] = v2;
         }
-        return edges.size()==n-1?true:false;
-        
+
+        return edges.size() == n - 1;
     }
-    int find(const vector<int>& uf,int node){
-        if(uf[node]==-1)
-            return node;
-        else
-            return find(uf,uf[node]);
+    int find(const vector<int>& uf, int id) {
+        if (uf[id] == -1) {
+            return id;
+        }
+
+        return find(uf, uf[id]);
     }
 };
 
-class Solution2 {
+class Solution {
 public:
     bool validTree(int n, vector<pair<int, int>>& edges) {
-        vector<int> uf(n,0);
-        for(int i=0;i<n;++i)
-            uf[i]=i;
-        for(auto&& e:edges){
-            int l=find(uf,e.first);
-            int r=find(uf,e.second);
-            if(l!=r){
-                uf[l]=r;
-            }
-            else
+        vector<int> uf(n, 0);
+        for (int i = 0; i < n; ++i) {
+            uf[i] = i;
+        }
+
+        for (auto&& e : edges) {
+            int v1 = find(uf, e.first);
+            int v2 = find(uf, e.second);
+            if (v1 == v2) {
                 return false;
+            }
+
+            uf[v1] = v2;
         }
-        return edges.size()==n-1;
-        
+
+        return edges.size() == n - 1;
     }
-    int find(vector<int>& uf,int id){
-        while(uf[id]!=id){
-            uf[id]=uf[uf[id]];//path compression.
-            id=uf[id];
+
+    int find(vector<int>& uf, int id) {
+        while (uf[id] != id) {
+            uf[id] = uf[uf[id]];
+            id = uf[id];
         }
+
         return id;
     }
 };
-
-int main(){
-}
+int main() {}
