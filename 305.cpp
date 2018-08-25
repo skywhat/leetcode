c6f65755fe358d042b1e9ec68e5335fb3a649c3a
skywhat <i@skywhat.com>
2018-08-24 21:29:56 -0400

    305. Number of Islands II

--- 305.cpp
+++ 305.cpp
@@ -1,48 +1,51 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-private:
-    vector<vector<int>> dir={{-1,0},{1,0},{0,1},{0,-1}};
 public:
+    vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     vector<int> numIslands2(int m, int n, vector<pair<int, int>>& positions) {
-        vector<int> root(m*n,-1);
+        vector<int> uf(m * n, -1);
         vector<int> res;
-        int cnt=0;
-        for(auto p:positions){
+        int cnt = 0;
+
+        for (auto p : positions) {
             cnt++;
-            int cur=p.first*n+p.second;
-            root[cur]=cur;
-            for(auto d:dir){
-                int x=p.first+d[0];
-                int y=p.second+d[1];
-                int nbor=x*n+y;
-                if(x<0||x>=m||y<0||y>=n||root[nbor]==-1)//not valid
+            int cur = p.first * n + p.second;
+            uf[cur] = cur;
+
+            for (auto d : dir) {
+                int x = p.first + d[0];
+                int y = p.second + d[1];
+                int nb = x * n + y;
+                if (x < 0 || x >= m || y < 0 || y >= n || uf[nb] == -1) {
                     continue;
-                int rootNbor=find(root,nbor);
-                if(find(root,cur)!=rootNbor){
-                    cnt--;
-                    root[cur]=rootNbor;// union two disjoint set
-                    cur=rootNbor;//update the cur to parent
                 }
-                
+
+                int cur_root = find(uf, cur);
+                int nb_root = find(uf, nb);
+                if (cur_root != nb_root) {
+                    cnt--;
+                    uf[nb_root] = cur_root;
+                }
             }
+
             res.push_back(cnt);
         }
+
         return res;
     }
-    int find(vector<int>& root,int id){
-        while(root[id]!=id){
-            root[id]=root[root[id]];//path compression lg(n)
-            id=root[id];
+
+    int find(vector<int>& uf, int id) {
+        while (uf[id] != id) {
+            uf[id] = uf[uf[id]]; // path compression
+            id = uf[id];
         }
+
         return id;
     }
 };
 
-int main(){
-    
-    
-}
+int main() {}
