740b65e24a41d21e55feed20155b2a0133ec7156
skywhat <i@skywhat.com>
2018-08-26 18:43:49 -0400

    139. Word Break

--- 139.cpp
+++ 139.cpp
@@ -1,40 +1,66 @@
-#include<string>
-#include<iostream>
-#include<unordered_set>
-#include<vector>
+/*
+Given a non-empty string s and a dictionary wordDict containing a list of
+non-empty words, determine if s can be segmented into a space-separated sequence
+of one or more dictionary words.
+
+Note:
+
+The same word in the dictionary may be reused multiple times in the
+segmentation. You may assume the dictionary does not contain duplicate words.
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet", "code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple", "pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen
+apple". Note that you are allowed to reuse a dictionary word. Example 3:
+
+Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output: false
+*/
+
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
 
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-	bool wordBreak(string s,vector<string>& wordDict){
-		vector<bool> b(s.size()+1,false);
-		b[0]=true;
-		unordered_set<string> set(wordDict.begin(),wordDict.end());
-		for(int i=1;i<=s.size();++i){
-			for(int j=i-1;j>=0;--j){
-				if(b[j]){
-					auto found=set.find(s.substr(j,i-j));
-					if(found!=set.end()){
-						b[i]=true;
-						break;
-					}
-				}
-			}
-		}
-		return b[s.size()];
-	}
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> words(wordDict.begin(), wordDict.end());
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = i - 1; j >= 0; --j) {
+                if (dp[j]) {
+                    if (words.count(s.substr(j, i - j))) {
+                        dp[i] = 1;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return dp[n];
+    }
 };
 
-int main(){
-	string test="leetcode";
-	vector<string> v={"leet","code"};
+int main() {
+    string test = "leetcode";
+    vector<string> v = {"leet", "code"};
 
-	string test1="goalspecial";
-	vector<string> v1={"go","goal","special"};
+    string test1 = "goalspecial";
+    vector<string> v1 = {"go", "goal", "special"};
 
-	Solution s;
-	cout<<s.wordBreak(test1,v1)<<endl;
+    Solution s;
+    cout << s.wordBreak(test1, v1) << endl;
 
-	return 0;
+    return 0;
 }
