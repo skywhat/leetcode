1b6d2e8fe6b47ff5da164f227e26bfce2b946971
skywhat <i@skywhat.com>
2018-08-27 11:03:01 -0400

    62. Unique Paths

--- 62.cpp
+++ 62.cpp
@@ -1,45 +1,45 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        if(m>n)
-        return uniquePaths(n,m);
-        vector<int> path(m+1,0);
-        path[1]=1;
-        for(int j=1;j<=n;++j){
-            for(int i=2;i<=m;++i){
-                path[i]=path[i]+path[i-1];
+        if (m > n) {
+            return uniquePaths(n, m);
+        }
+
+        vector<int> cur(m, 1);
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < m; ++j) {
+                cur[j] += cur[j - 1];
             }
         }
-        return path[m];
+
+        return cur[m - 1];
     }
 };
 
-class Solution2 {
+class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
-        for(int i=1;i<=m;++i)
-        dp[i][1]=1;
-        for(int j=1;j<=n;++j)
-        dp[1][j]=1;
-        for(int i=2;i<=m;++i){
-            for(int j=2;j<=n;++j){
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        vector<vector<int>> dp(n, vector<int>(m, 1));
+
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < m; ++j) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
-        return dp[m][n];
+
+        return dp[n - 1][m - 1];
     }
 };
 
-int main(){
-	int m=3,n=4;
-	Solution s;
-	cout<<s.uniquePaths(m,n)<<endl;
+int main() {
+    int m = 3, n = 4;
+    Solution s;
+    cout << s.uniquePaths(m, n) << endl;
 
-	return 0;
+    return 0;
 }
