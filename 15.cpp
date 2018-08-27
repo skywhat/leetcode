15879f3575114c56636c268cc3da99d28aff5b40
skywhat <i@skywhat.com>
2018-08-27 14:35:25 -0400

    15. 3Sum

--- 15.cpp
+++ 15.cpp
@@ -1,67 +1,95 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
+/*
+Given an array nums of n integers, are there elements a, b, c in nums such that
+a + b + c = 0? Find all unique triplets in the array which gives the sum of
+zero.
+
+Note:
+
+The solution set must not contain duplicate triplets.
+
+Example:
+
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums){
+    vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
-        int n=nums.size();
-        if(n<3)
+        int n = nums.size();
+        if (n < 3) {
             return res;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i+2<nums.size();++i){
-            //nums[i-1]!=nums[i] no more duplicate triplets exists
-            //nums[i]<=0 the first element in triplet is obviously less than or equal to 0
-            if(nums[i]<=0&&(i==0||(i>0&&nums[i-1]!=nums[i]))){
-                if(nums[i]+nums[i+1]+nums[i+2]>0)
-                    break;
-                if(nums[i]+nums[n-1]+nums[n-2]<0)
-                    continue;
-                int start=i+1,end=nums.size()-1;
-                int sum=0-nums[i];
-                while(start<end){
-                    if(nums[start]+nums[end]>sum){
-                        end--;
-                    }
-                    else if(nums[start]+nums[end]<sum){
+        }
+
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i + 2 < n; ++i) {
+            if (nums[i] > 0) {
+                continue;
+            }
+
+            if (i > 0 && (nums[i] == nums[i - 1])) {
+                continue;
+            }
+
+            if (nums[i] + nums[i + 1] + nums[i + 2] > 0) {
+                break;
+            }
+
+            if (nums[i] + nums[n - 2] + nums[n - 1] < 0) {
+                continue;
+            }
+
+            int start = i + 1, end = n - 1;
+            while (start < end) {
+                if (nums[i] + nums[start] + nums[end] < 0) {
+                    start++;
+                } else if (nums[i] + nums[start] + nums[end] > 0) {
+                    end--;
+                } else {
+                    res.push_back({nums[i], nums[start], nums[end]});
+                    start++;
+                    while (start < end && (nums[start - 1] == nums[start])) {
                         start++;
                     }
-                    else{
-                        res.push_back({nums[i],nums[start],nums[end]});
-                        int prev=nums[start];
-                        while(start<end&&prev==nums[start])
-                            start++;
-                        prev=nums[end];
-                        while(start<end&&prev==nums[end])
-                            end--;
+                    end--;
+                    while (start < end && (nums[end + 1] == nums[end])) {
+                        end--;
                     }
                 }
             }
         }
+
         return res;
     }
 };
-//Your runtime beats 99.85 % of cpp submissions.
 
-int main(){
-    vector<int> test={-1,0,1,0};
-    vector<int> test1={-1,0,1,2,-1,4};
+int main() {
+    vector<int> test = {-1, 0, 1, 0};
+    vector<int> test1 = {-1, 0, 1, 2, -1, 4};
     Solution s;
-    vector<vector<int>> res=s.threeSum(test);
-    for(auto row:res){
-        cout<<"[";
-        for(auto elem:row){
-            cout<<elem<<", ";
+    vector<vector<int>> res = s.threeSum(test);
+    for (auto row : res) {
+        cout << "[";
+        for (auto elem : row) {
+            cout << elem << ", ";
         }
-        cout<<"]"<<endl;
+        cout << "]" << endl;
     }
-    
+
     return 0;
 }
-
 
 /*
  *follow up:
