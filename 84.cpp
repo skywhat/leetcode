327cf5a3cc5901afd0f3bad312e4e96d11806ed6
skywhat <i@skywhat.com>
2018-08-27 21:53:50 -0400

    84. Largest Rectangle in Histogram

--- 84.cpp
+++ 84.cpp
@@ -1,5 +1,6 @@
 /*
-Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+Given n non-negative integers representing the histogram's bar height where the
+width of each bar is 1, find the area of largest rectangle in the histogram.
 
 
 
@@ -17,50 +18,84 @@ For example,
 Given heights = [2,1,5,6,2,3],
 return 10.
 */
-#include<iostream>
-#include<vector>
-#include<stack>
+#include <iostream>
+#include <stack>
+#include <vector>
 
 using namespace std;
+
+// O(n^3) Time Limit Exceeded
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int max_area = 0;
+
+        for (int i = 0; i < heights.size(); ++i) {
+            for (int j = i; j < heights.size(); ++j) {
+                int height = INT_MAX;
+                for (int k = i; k <= j; ++k) {
+                    height = min(height, heights[k]);
+                }
+                max_area = max(max_area, (j - i + 1) * height);
+            }
+        }
+
+        return max_area;
+    }
+};
+
+// O(n^2)
+class Solution2 {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int max_area = 0;
+
+        for (int i = 0; i < heights.size(); ++i) {
+            int left = i, right = i;
+            while (left >= 0 && heights[left] >= heights[i]) {
+                left--;
+            }
+            while (right < heights.size() && heights[right] >= heights[i]) {
+                right++;
+            }
+            max_area = max(max_area, (right - left - 1) * heights[i]);
+        }
+
+        return max_area;
+    }
+};
 /*
 The key point is recording the index of histogram bars in non-descending order.
 Only when we find the minmum height, the stack will be empty.
 */
-class Solution {
+class Solution3 {
 public:
-	int largestRectangleArea(vector<int>& heights) {
-		heights.push_back(0);
-		//we need the index of end over the last period of non-descending order
-		stack<int> s;
-		int maxArea = 0;
-		int Area;
-		int width;
-		for (int i = 0; i < heights.size(); ) {
-			if(s.empty() || heights[i] > heights[s.top()]) {
-				s.push(i);
-				i++;
-			}
-			else {
-				int index = s.top();
-				s.pop();
-				if (s.empty())
-					width = i;
-				//the bar which has the minumu height.
-				else
-					width = i -s.top() - 1;
-				//the left index is s.top()
-				Area = heights[index] * width;
-				if (Area > maxArea)
-					maxArea = Area;
-			}
-		}
-		return maxArea;
-	}
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> index;
+        // we need the index of end over the last period of non-descending order
+        heights.push_back(0);
+        int max_area = 0;
+
+        int i = 0;
+        while (i < heights.size()) {
+            if (index.empty() || heights[index.top()] <= heights[i]) {
+                index.push(i);
+                i++;
+            } else {
+                int h = heights[index.top()];
+                index.pop();
+                int left_index = index.empty() ? -1 : index.top();
+                max_area = max(max_area, (i - left_index - 1) * h);
+            }
+        }
+
+        return max_area;
+    }
 };
 
 int main() {
-	Solution s;
-	vector<int> h = {4,2,0,3,2,5};
-	cout << s.largestRectangleArea(h) << endl;
-	return 0;
+    Solution s;
+    vector<int> h = {4, 2, 0, 3, 2, 5};
+    cout << s.largestRectangleArea(h) << endl;
+    return 0;
 }
