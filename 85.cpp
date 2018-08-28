b8e799432adf3e764a47e346e60302db0e27fcf2
skywhat <i@skywhat.com>
2018-08-28 10:46:49 -0400

    85. Maximal Rectangle

--- 85.cpp
+++ 85.cpp
@@ -1,53 +1,71 @@
-#include<iostream>
-#include<stack>
-#include<vector>
+/*
+Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle
+containing only 1's and return its area.
+
+Example:
+
+Input:
+[
+  ["1","0","1","0","0"],
+  ["1","0","1","1","1"],
+  ["1","1","1","1","1"],
+  ["1","0","0","1","0"]
+]
+Output: 6
+*/
+
+#include <iostream>
+#include <stack>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        if(matrix.size()==0)
+        int m = matrix.size();
+        if (!m) {
             return 0;
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int maxArea=0;
-        vector<int> heights(n+1,0);
-        
-        for(int i=0;i<m;++i){
-            for(int j=0;j<n;++j){
-                if(matrix[i][j]=='0')
-                    heights[j]=0;
-                else
+        }
+        int n = matrix[0].size();
+        vector<int> heights(n + 1, 0);
+        int max_area = 0;
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == '0') {
+                    heights[j] = 0;
+                } else {
                     heights[j]++;
+                }
             }
-            maxArea=max(maxArea,maxHist(heights));
+            copy(heights.begin(), heights.end(),
+                 ostream_iterator<int>(cout, ", "));
+            max_area = max(max_area, maxHistArea(heights));
         }
-        return maxArea;
+
+        return max_area;
     }
-    int maxHist(const vector<int>& heights){
-        stack<int> s;
-        int h,width,maxArea=0;
-        for(int i=0;i<heights.size();){
-            if(s.empty()||heights[s.top()]<=heights[i]){
-                s.push(i);
+
+    int maxHistArea(const vector<int>& heights) {
+        stack<int> index;
+        int max_area = 0;
+
+        int i = 0;
+        while (i < heights.size()) {
+            if (index.empty() || heights[index.top()] <= heights[i]) {
+                index.push(i);
                 i++;
-            }
-            else{
-                h=heights[s.top()];
-                s.pop();
-                if(s.empty()){
-                    width=i;
-                }
-                else{
-                    width=i-s.top()-1;
-                }
-                maxArea=max(width*h,maxArea);
+            } else {
+                int h = heights[index.top()];
+                index.pop();
+                int left_index = index.empty() ? -1 : index.top();
+                max_area = max(max_area, h * (i - left_index - 1));
             }
         }
-        return maxArea;
+
+        return max_area;
     }
 };
 
-int main(){
-}
+int main() {}
