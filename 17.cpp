00cce9fb02e52390c17a922a59a31f7635372fdf
skywhat <i@skywhat.com>
2018-08-28 23:43:03 -0400

    17. Letter Combinations of a Phone Number

--- 17.cpp
+++ 17.cpp
@@ -1,55 +1,60 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-
-//recursive
+// recursive
 class Solution {
 public:
+    vector<string> digit_to_letters
+        = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     vector<string> letterCombinations(string digits) {
         vector<string> res;
-        vector<string> m={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        if(digits.size()==0)
-            return res;
-        combine(digits,0,res,m,"");
+        if (digits.empty()) {
+            return {};
+        }
+
+        combine("", res, 0, digits);
         return res;
     }
-    void combine(string digits,int pos,vector<string>& res,vector<string>& m,string tmp){
-        if(pos==digits.size()){
-            res.push_back(tmp);
+
+    void combine(string temp, vector<string>& res, int pos, string digits) {
+        if (pos >= digits.size()) {
+            res.push_back(temp);
             return;
         }
-        for(auto c:m[digits[pos]-'0']){
-            combine(digits,pos+1,res,m,tmp+c);
+
+        for (auto c : digit_to_letters[digits[pos] - '0']) {
+            combine(temp + c, res, pos + 1, digits);
         }
     }
-    
 };
 
-//iterative
+// iterative
 class Solution2 {
 public:
+    vector<string> digit_to_letters
+        = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     vector<string> letterCombinations(string digits) {
         vector<string> res;
-        if(digits.size()==0)
-            return res;
-        vector<string> m={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        if (digits.empty()) {
+            return {};
+        }
+
         res.push_back("");
-        for(int i=0;i<digits.size();++i){
-            vector<string> tmp;
-            for(int j=0;j<m[digits[i]-'0'].size();++j){
-                for(int k=0;k<res.size();++k){
-                    tmp.push_back(res[k]+m[digits[i]-'0'][j]);
+        for (int i = 0; i < digits.size(); ++i) {
+            vector<string> temp;
+            for (auto c : digit_to_letters[digits[i] - '0']) {
+                for (int i = 0; i < res.size(); ++i) {
+                    temp.push_back(res[i] + c);
                 }
             }
-            res=tmp;
+            res = temp;
         }
+
         return res;
     }
 };
 
-int main(){
-    
-}
+int main() {}
