3cc5d163716cf6d212982105efbfe24e22c4f713
skywhat <i@skywhat.com>
2018-08-30 18:06:09 -0400

    40. Combination Sum II

--- 40.cpp
+++ 40.cpp
@@ -1,28 +1,105 @@
+/*
+Given a collection of candidate numbers (candidates) and a target number
+(target), find all unique combinations in candidates where the candidate numbers
+sums to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note:
+
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+Example 1:
+
+Input: candidates = [10,1,2,7,6,1,5], target = 8,
+A solution set is:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+Example 2:
+
+Input: candidates = [2,5,2,1,2], target = 5,
+A solution set is:
+[
+  [1,2,2],
+  [5]
+]
+
+*/
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
         vector<vector<int>> res;
-        vector<int> tmp;
-        sort(candidates.begin(),candidates.end());
-        combine(candidates,target,res,tmp,0);
+        vector<int> temp;
+        combine(candidates, target, 0, temp, res);
         return res;
     }
-    void combine(vector<int>& candidates,int target,vector<vector<int>>& res,vector<int>& tmp,int pos){
-        if(target<0)
-            return;
-        if(target==0){
-            res.push_back(tmp);
+
+    void combine(vector<int>& candidates,
+                 int target,
+                 int pos,
+                 vector<int>& temp,
+                 vector<vector<int>>& res) {
+        if (target == 0) {
+            res.push_back(temp);
             return;
         }
-        for(int i=pos;i<candidates.size();){
-            tmp.push_back(candidates[i]);
-            combine(candidates,target-candidates[i],res,tmp,i+1);
-            tmp.pop_back();
-            int cnt=0;
-            while(i+cnt<candidates.size()&&candidates[i+cnt]==candidates[i])
+
+        if (target < 0) {
+            return;
+        }
+
+        int i = pos;
+        while (i < candidates.size()) {
+            temp.push_back(candidates[i]);
+            combine(candidates, target - candidates[i], i + 1, temp, res);
+            temp.pop_back();
+
+            int cnt = 1;
+            while (cnt + i < candidates.size()
+                   && candidates[i] == candidates[i + cnt]) {
                 cnt++;
-            i+=cnt;
+            }
+            i += cnt;
         }
     }
-    
+};
+
+class Solution2 {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> res;
+        vector<int> temp;
+        combine(candidates, target, 0, temp, res);
+        return res;
+    }
+
+    void combine(vector<int>& candidates,
+                 int target,
+                 int pos,
+                 vector<int>& temp,
+                 vector<vector<int>>& res) {
+        if (target == 0) {
+            res.push_back(temp);
+            return;
+        }
+
+        if (target < 0) {
+            return;
+        }
+
+        for (int i = pos; i < candidates.size(); ++i) {
+            if (i > pos && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            temp.push_back(candidates[i]);
+            combine(candidates, target - candidates[i], i + 1, temp, res);
+            temp.pop_back();
+        }
+    }
 };
