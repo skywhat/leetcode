2c1a50ebb2b7b838ca8738cc56871c9daf1c326c
skywhat <i@skywhat.com>
2018-09-03 21:24:46 -0400

    auto-format

--- 680.cpp
+++ 680.cpp
@@ -1,43 +1,39 @@
-#include<iostream>
-#include<vector>
-#include<string>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     bool validPalindrome(string s) {
-        int i=0,j=s.size()-1;
-        while(i<j){
-            if(s[i] != s[j]){
-                return valid(s, i+1, j) || valid(s, i, j-1);
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            if (s[i] != s[j]) {
+                return valid(s, i + 1, j) || valid(s, i, j - 1);
             }
             i++;
             j--;
         }
-        
+
         return true;
     }
-    
-    bool valid(string s, int left, int right){
-        while(left < right){
-            if(s[left++] != s[right--]){
+
+    bool valid(string s, int left, int right) {
+        while (left < right) {
+            if (s[left++] != s[right--]) {
                 return false;
             }
         }
-        
+
         return true;
     }
 };
 
-int main(){
-	Solution s;
-	string test="abca";
-	cout<<s.validPalindrome(test)<<endl;
+int main() {
+    Solution s;
+    string test = "abca";
+    cout << s.validPalindrome(test) << endl;
 
-	return 0;
+    return 0;
 }
-
-
-
-
