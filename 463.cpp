35b8da7942b70fe0b94e5701cb9db271929a5601
skywhat <i@skywhat.com>
2018-09-04 21:54:13 -0400

    463. Island Perimeter

--- 463.cpp
+++ 463.cpp
@@ -1,51 +1,54 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    int islandPerimeter(vector<vector<int>>& grid) {
-        if(grid.empty()){
-            return 0;
-        }
-        
-        int brick = 0, seam = 0;
-        for(int i=0;i<grid.size();++i){
-            for(int j=0;j<grid[0].size();++j){
-                if(grid[i][j]){
-                    brick++;
-                    if(i-1>=0 && grid[i-1][j]){
-                        seam++;
-                    }
-                    if(i+1<grid.size() && grid[i+1][j]){
-                        seam++;
-                    }
-                    if(j-1>=0 && grid[i][j-1]){
-                        seam++;
-                    }
-                    if(j+1<grid[0].size() && grid[i][j+1]){
-                        seam++;
+    class Solution {
+    public:
+        vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int islandPerimeter(vector<vector<int>>& grid) {
+            int m = grid.size();
+            if (!m) {
+                return 0;
+            }
+            int n = grid[0].size();
+
+            int brick = 0;
+            int seam = 0;
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (grid[i][j]) {
+                        brick++;
+                        for (auto d : dir) {
+                            int x = i + d[0];
+                            int y = j + d[1];
+                            if (x < 0 || x >= m || y < 0 || y >= n) {
+                                continue;
+                            }
+                            if (grid[x][y]) {
+                                seam++;
+                            }
+                        }
                     }
                 }
             }
+
+            return brick * 4 - seam;
         }
-        
-        return 4*brick - seam;
+    };
+
+    int main() {
+        vector<vector<int>> grid = {
+            {0, 1, 0, 0},
+            {1, 1, 1, 0},
+            {0, 1, 0, 0},
+            {1, 1, 0, 0},
+        };
+
+        int expect = 16;
+        Solution s;
+        cout << (s.islandPerimeter(grid) == expect) << endl;
+
+        return 0;
     }
-};
-
-int main(){
-	vector<vector<int>> grid = {
-		{0,1,0,0},
-		{1,1,1,0},
-		{0,1,0,0},
-		{1,1,0,0},
-	};
-
-	int expect = 16;
-	Solution s;
-	cout<<(s.islandPerimeter(grid) == expect) <<endl;
-
-	return 0;
-}
