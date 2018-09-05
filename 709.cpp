7884745940591ac84d5f34ef3ac41ef6b5878ef4
skywhat <i@skywhat.com>
2018-09-04 22:34:51 -0400

    709. To Lower Case

---
+++ 709.cpp
@@ -0,0 +1,29 @@
+// Implement function ToLowerCase() that has a string parameter str, and returns
+// the same string in lowercase.
+//
+//
+//
+// Example 1:
+//
+// Input: "Hello"
+// Output: "hello"
+// Example 2:
+//
+// Input: "here"
+// Output: "here"
+// Example 3:
+//
+// Input: "LOVELY"
+// Output: "lovely"
+
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for (auto& c : str) {
+            if (isupper(c)) {
+                c = tolower(c);
+            }
+        }
+        return str;
+    }
+};
