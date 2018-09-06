a7d8da3e181bc13205b2fe82b16064e2c7c786e6
skywhat <i@skywhat.com>
2018-09-05 21:38:39 -0400

    227. Basic Calculator II

--- 227.cpp
+++ 227.cpp
@@ -1,37 +1,73 @@
-#include<iostream>
-#include<string>
-#include<sstream>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
 class Solution {
 public:
     int calculate(string s) {
-        s='+'+s+'+';
+        s = '+' + s + '+';
         istringstream in(s);
+        int res = 0;
+        int term = 0;
         char op;
-        int term=0;
-        int res=0;
-        int num;
-        while(in>>op){
-            if(op=='+'||op=='-'){
-                res+=term;
-                in>>term;
-                term*=(op=='+'?1:-1);
-            }
-            else{
-                in>>num;
-                if(op=='*')
-                    term*=num;
-                else
-                    term/=num;
+
+        while (in >> op) {
+            if (op == '+' || op == '-') {
+                res += term;
+                in >> term;
+                if (op == '-') {
+                    term = -term;
+                }
+            } else {
+                int temp;
+                in >> temp;
+                if (op == '*') {
+                    term *= temp;
+                } else {
+                    term /= temp;
+                }
             }
         }
+
         return res;
     }
 };
 
+class Solution2 {
+public:
+    int calculate(string s) {
+        stack<int> nums;
+        char sign = '+';
+        int n = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (isdigit(s[i])) {
+                n = n * 10 + s[i] - '0';
+            }
+            if (!isspace(s[i]) && !isdigit(s[i]) || i == s.size() - 1) {
+                if (sign == '+') {
+                    nums.push(n);
+                } else if (sign == '-') {
+                    nums.push(-n);
+                } else if (sign == '*' || sign == '/') {
+                    int res = sign == '*' ? nums.top() * n : nums.top() / n;
+                    nums.pop();
+                    nums.push(res);
+                }
+                n = 0;
+                sign = s[i];
+            }
+        }
 
-int main(){
+        int res = 0;
+        while (!nums.empty()) {
+            res += nums.top();
+            nums.pop();
+        }
 
-}
+        return res;
+    }
+};
+
+int main() {}
