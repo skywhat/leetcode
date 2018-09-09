9e71c29b73688999fe988598f2f19148ae580dd6
skywhat <i@skywhat.com>
2018-09-08 20:56:56 -0400

    5. Longest Palindromic Substring

--- 5.cpp
+++ 5.cpp
@@ -1,38 +1,37 @@
-#include<iostream>
-#include<string>
-#include<algorithm>
+#include <algorithm>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-	string longestPalindrome(string s){
-		int start=0,max_len=1;
-		for(int i=0;i<s.size();++i){
-			if(s.size()-i<max_len/2)
-				break;
-			int left=i,right=i;
-			while(right+1<s.size()&&(s[right+1]==s[right])){
-				right++;
-			}
-			
-			while(left>0&&right+1<s.size()&&(s[left-1]==s[right+1])){
-				left--;
-				right++;
-			}
-			if(right-left+1>max_len){
-				start=left;
-				max_len=right-left+1;
-			}
-		}
-		return s.substr(start,max_len);
-	}
+    string longestPalindrome(string s) {
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int start = 0;
+        int maxLen = 0;
+
+        for (int j = 0; j < n; ++j) {
+            for (int i = 0; i <= j; ++i) {
+                dp[i][j]
+                    = (s[i] == s[j]) && (i + 1 >= j - 1 ? 1 : dp[i + 1][j - 1]);
+
+                if (dp[i][j] && (maxLen <= j - i + 1)) {
+                    maxLen = j - i + 1;
+                    start = i;
+                }
+            }
+        }
+
+        return s.substr(start, maxLen);
+    }
 };
 
-int main(){
-	Solution s;
-	string test="babad";
-	cout<<s.longestPalindrome(test)<<endl;
+int main() {
+    Solution s;
+    string test = "babad";
+    cout << s.longestPalindrome(test) << endl;
 
-	return 0;
+    return 0;
 }
