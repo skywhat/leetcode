e7fc1ad9155f3c674a8039f6e54069aae85eff0c
skywhat <i@skywhat.com>
2018-09-09 23:47:10 -0400

    96. Unique Binary Search Trees

--- 96.cpp
+++ 96.cpp
@@ -1,29 +1,44 @@
-#include<iostream>
-#include<vector>
+// Given n, how many structurally unique BST's (binary search trees) that store
+// values 1 ... n?
+//
+// Example:
+//
+// Input: 3
+// Output: 5
+// Explanation:
+// Given n = 3, there are a total of 5 unique BST's:
+//
+//    1         3     3      2      1
+//     \       /     /      / \      \
+//      3     2     1      1   3      2
+//     /     /       \                 \
+//    2     1         2                 3
+//
+
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-	int numTrees(int n){
-		vector<int> res(n+1,0);
-		if(n>=1){
-			res[0]=1;
-			res[1]=1;
-		}
-		for(int i=2;i<=n;++i){
-			for(int j=0;j<=i-1;++j){
-				res[i]+=res[j]*res[i-1-j];
-			}
-		}
-		return res[n];
-	}
+    int numTrees(int n) {
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+
+        return dp[n];
+    }
 };
 
-int main(){
-	Solution s;
-	int n=3;
-	cout<<s.numTrees(n)<<endl;
+int main() {
+    Solution s;
+    int n = 3;
+    cout << s.numTrees(n) << endl;
 
-	return 0;
+    return 0;
 }
