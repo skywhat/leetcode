9295b14f5a49c08991f939f90d55c853e1aec25b
skywhat <i@skywhat.com>
2018-09-14 02:16:45 -0400

    45. Jump Game II

--- 45.cpp
+++ 45.cpp
@@ -1,38 +1,58 @@
-#include<iostream>
-#include<vector>
+// Given an array of non-negative integers, you are initially positioned at the
+// first index of the array.
+//
+// Each element in the array represents your maximum jump length at that
+// position.
+//
+// Your goal is to reach the last index in the minimum number of jumps.
+//
+// Example:
+//
+// Input: [2,3,1,1,4]
+// Output: 2
+// Explanation: The minimum number of jumps to reach the last index is 2.
+//     Jump 1 step from index 0 to 1, then 3 steps to the last index.
+// Note:
+//
+// You can assume that you can always reach the last index.
+
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-	int jump(vector<int>& nums){
-		int step=0;
-		int maxDist=0;
-		int next=0;
-		int last=nums.size()-1;
-		for(int i=0;i<last;++i){
-			maxDist=max(maxDist,i+nums[i]);
-			if(i==next){
-				step++;
-				next=maxDist;
-			}
-			if(maxDist>=last&&next<last)
-				return step+1;
-			else if(maxDist>=last&&next>=last)
-				return step;
-		}
-		return step;
-	}
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 2) {
+            return 0;
+        }
+
+        int step = 0;
+        int next = 0;
+        int max_des = 0;
+
+        for (int i = 0; i < n - 1; ++i) {
+            max_des = max(max_des, i + nums[i]);
+            if (i == next) {
+                step++;
+                next = max_des;
+            }
+
+            if (max_des >= n - 1) {
+                return step + (next >= n - 1 ? 0 : 1);
+            }
+        }
+
+        return -1;
+    }
 };
 
-int main(){
-	vector<int> test={2,3,1,1,4};
+int main() {
+    vector<int> test = {2, 3, 1, 1, 4};
+    Solution s;
+    cout << s.jump(test) << endl;
 
-	Solution s;
-	cout<<s.jump(test)<<endl;
-
-	return 0;
+    return 0;
 }
-
-
-
