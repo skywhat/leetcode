bd293752d29c5d7106e4a17b692a8cf8b9fd19e7
skywhat <i@skywhat.com>
2018-09-14 03:10:53 -0400

    116. Populating Next Right Pointers in Each Node

--- 116.cpp
+++ 116.cpp
@@ -1,53 +1,108 @@
-#include<iostream>
-#include<vector>
+// Given a binary tree
+//
+// struct TreeLinkNode {
+//   TreeLinkNode *left;
+//   TreeLinkNode *right;
+//   TreeLinkNode *next;
+// }
+// Populate each next pointer to point to its next right node. If there is no
+// next right node, the next pointer should be set to NULL.
+//
+// Initially, all next pointers are set to NULL.
+//
+// Note:
+//
+// You may only use constant extra space.
+// Recursive approach is fine, implicit stack space does not count as extra
+// space for this problem. You may assume that it is a perfect binary tree (ie,
+// all leaves are at the same level, and every parent has two children).
+// Example:
+//
+// Given the following perfect binary tree,
+//
+//      1
+//    /  \
+//   2    3
+//  / \  / \
+// 4  5  6  7
+// After calling your function, the tree should look like:
+//
+//      1 -> NULL
+//    /  \
+//   2 -> 3 -> NULL
+//  / \  / \
+// 4->5->6->7 -> NULL
+
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 struct TreeLinkNode {
-int val;
-TreeLinkNode *left, *right, *next;
-TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+    int val;
+    TreeLinkNode *left, *right, *next;
+    TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
 };
 
 class Solution {
 public:
-    void connect(TreeLinkNode *root) {
-        if(!root)
-            return;
-        TreeLinkNode* prev=root;
-        TreeLinkNode* cur=nullptr;
-        while(prev->left){
-            cur=prev;
-            while(cur){
-                cur->left->next=cur->right;
-                if(cur->next)
-                    cur->right->next=cur->next->left;
-                cur=cur->next;
+    void connect(TreeLinkNode* root) {
+        while (root && root->left) {
+            TreeLinkNode* cur = root;
+            while (cur) {
+                cur->left->next = cur->right;
+                if (cur->next) {
+                    cur->right->next = cur->next->left;
+                }
+                cur = cur->next;
             }
-            prev=prev->left;
+            root = root->left;
         }
     }
 };
 
 class Solution2 {
 public:
-    void connect(TreeLinkNode *root) {
+    void connect(TreeLinkNode* root) {
         queue<TreeLinkNode*> q;
-        if(root)
+        if (root) {
             q.push(root);
-        while(!q.empty()){
-            int size=q.size();
-            while(size--){
-                TreeLinkNode* tmp=q.front();
+        }
+
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                auto node = q.front();
                 q.pop();
-                if(size>0){
-                    tmp->next=q.front();
+                if (size > 0) {
+                    node->next = q.front();
                 }
-                if(tmp->left)
-                    q.push(tmp->left);
-                if(tmp->right)
-                    q.push(tmp->right);
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
             }
         }
+    }
+};
+
+class Solution3 {
+public:
+    void connect(TreeLinkNode* root) {
+        if (!root) {
+            return;
+        }
+
+        if (root->left) {
+            root->left->next = root->right;
+            if (root->next) {
+                root->right->next = root->next->left;
+            }
+        }
+
+        connect(root->left);
+        connect(root->right);
     }
 };
