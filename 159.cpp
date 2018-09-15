721179262cba44d9c92f53764edf217f4ee0cc59
skywhat <i@skywhat.com>
2018-09-15 00:57:22 -0400

    159. Longest Substring with At Most Two Distinct Characters

--- 159.cpp
+++ 159.cpp
@@ -1,27 +1,35 @@
-#include<iostream>
-#include<vector>
-#include<string>
-
-using namespace std;
+// Given a string s , find the length of the longest substring t  that contains
+// at most 2 distinct characters.
+//
+// Example 1:
+//
+// Input: "eceba"
+// Output: 3
+// Explanation: t is "ece" which its length is 3.
+// Example 2:
+//
+// Input: "ccaabbb"
+// Output: 5
+// Explanation: t is "aabbb" which its length is 5.
 
 class Solution {
 public:
     int lengthOfLongestSubstringTwoDistinct(string s) {
-        vector<int> m(128,0);
-        int j=-1;
-        int res=0;
-        int cnt=0;
-        for(int i=0;i<s.size();++i){
-            cnt+=m[s[i]]==0;
+        int max_cnt = 0;
+        int cnt = 0;
+        vector<int> m(128, 0);
+
+        for (int i = 0, j = -1; i < s.size(); ++i) {
+            cnt += m[s[i]] == 0;
             m[s[i]]++;
-            
-            while(cnt>2){
+            while (cnt > 2) {
                 j++;
                 m[s[j]]--;
-                cnt-=m[s[j]]==0;
+                cnt -= m[s[j]] == 0;
             }
-            res=max(res,i-j);
+            max_cnt = max(max_cnt, i - j);
         }
-        return res;
+
+        return max_cnt;
     }
 };
