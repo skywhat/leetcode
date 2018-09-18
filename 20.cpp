c77918daac21aaafcfe7ead72de23e103cdd35f4
skywhat <i@skywhat.com>
2018-09-18 14:24:39 -0400

    update

--- 20.cpp
+++ 20.cpp
@@ -1,43 +1,68 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<stack>
+// Given a string containing just the characters '(', ')', '{', '}', '[' and
+// ']', determine if the input string is valid.
+//
+// An input string is valid if:
+//
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Note that an empty string is also considered valid.
+//
+// Example 1:
+//
+// Input: "()"
+// Output: true
+// Example 2:
+//
+// Input: "()[]{}"
+// Output: true
+// Example 3:
+//
+// Input: "(]"
+// Output: false
+// Example 4:
+//
+// Input: "([)]"
+// Output: false
+// Example 5:
+//
+// Input: "{[]}"
+// Output: true
+
+
+#include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        int i=0;
-        while(i<s.size()){
-            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
-                st.push(s[i]);
-            }
-            else if(s[i] == ')'){
-                if(st.empty() || st.top() != '('){
+
+        for (auto c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            } else if (c == ')') {
+                if (st.empty() || st.top() != '(') {
+                    return false;
+                }
+                st.pop();
+            } else if (c == ']') {
+                if (st.empty() || st.top() != '[') {
+                    return false;
+                }
+                st.pop();
+            } else if (c == '}') {
+                if (st.empty() || st.top() != '{') {
                     return false;
                 }
                 st.pop();
             }
-            else if(s[i] == ']'){
-                if(st.empty() || st.top() != '['){
-                    return false;
-                }
-                st.pop();
-            }
-            else if(s[i] == '}'){
-                if(st.empty() || st.top() != '{'){
-                    return false;
-                }
-                st.pop();
-            }
-            i++;
         }
-        
+
         return st.empty();
     }
 };
 
-int main(){
-
-}
+int main() {}
