bc3688f3af727a0624465cc3252b266a72250e1d
skywhat <i@skywhat.com>
2018-09-23 16:26:57 -0400

    253. Meeting Rooms II

--- 253.cpp
+++ 253.cpp
@@ -1,5 +1,18 @@
-#include<iostream>
-#include<vector>
+// Given an array of meeting time intervals consisting of start and end times
+// [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms
+// required.
+//
+// Example 1:
+//
+// Input: [[0, 30],[5, 10],[15, 20]]
+// Output: 2
+// Example 2:
+//
+// Input: [[7,10],[2,4]]
+// Output: 1
+
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -16,23 +29,27 @@ using namespace std;
 class Solution {
 public:
     int minMeetingRooms(vector<Interval>& intervals) {
-        int n=intervals.size();
-        vector<int> start(n,0);
-        vector<int> end(n,0);
-        for(int i=0;i<intervals.size();++i){
-            start[i]=intervals[i].start;
-            end[i]=intervals[i].end;
+        int n = intervals.size();
+        vector<int> start(n, 0);
+        vector<int> end(n, 0);
+        for (int i = 0; i < intervals.size(); ++i) {
+            start[i] = intervals[i].start;
+            end[i] = intervals[i].end;
         }
-        sort(start.begin(),start.end());
-        sort(end.begin(),end.end());
-        int room=0;
-        int endindex=0;
-        for(int i=0;i<start.size();++i){
-            if(start[i]<end[endindex])
+
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+        int room = 0;
+        int endindex = 0;
+
+        for (int i = 0; i < start.size(); ++i) {
+            if (start[i] < end[endindex]) {
                 room++;
-            else
+            } else {
                 endindex++;
+            }
         }
+
         return room;
     }
 };
@@ -40,17 +57,21 @@ public:
 class Solution2 {
 public:
     int minMeetingRooms(vector<Interval>& intervals) {
-        map<int,int> m;
-        for(int i=0;i<intervals.size();++i){
+        map<int, int> m;
+
+        for (int i = 0; i < intervals.size(); ++i) {
             m[intervals[i].start]++;
             m[intervals[i].end]--;
         }
-        int maxRoom=0;
-        int sum=0;
-        for(auto it:m){
-            sum+=it.second;
-            maxRoom=max(maxRoom,sum);
+
+        int maxRoom = 0;
+        int sum = 0;
+
+        for (auto it : m) {
+            sum += it.second;
+            maxRoom = max(maxRoom, sum);
         }
+
         return maxRoom;
     }
 };
