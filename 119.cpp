e78ba8af4c29695b3e343d3336306a296e861f30
skywhat <i@skywhat.com>
2018-09-23 19:30:17 -0400

    119. Pascal's Triangle II

--- 119.cpp
+++ 119.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> getRow(int rowIndex) {
         vector<int> res(rowIndex + 1);
         res[0] = 1;
-        for (int i = 0; i <= rowIndex; ++i) {
+        for (int i = 1; i <= rowIndex; ++i) {
             for (int j = i; j >= 1; --j) {
                 res[j] += res[j - 1];
             }
