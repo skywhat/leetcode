2754e0fe02e07e6d0f3ac8a62dc0235a92864dbb
skywhat <i@skywhat.com>
2018-09-23 03:00:22 -0400

    auto-format

--- 242.cpp
+++ 242.cpp
@@ -1,42 +1,40 @@
 /*
- Given two strings s and t, write a function to determine if t is an anagram of s.
- 
+ Given two strings s and t, write a function to determine if t is an anagram of
+ s.
+
  For example,
  s = "anagram", t = "nagaram", return true.
  s = "rat", t = "car", return false.
- 
+
  Note:
  You may assume the string contains only lowercase alphabets.
  */
 
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()){
+        if (s.size() != t.size()) {
             return false;
         }
-        
+
         vector<int> m(128, 0);
-        for(auto c: s){
+        for (auto c : s) {
             m[c]++;
         }
-        
-        for(auto c: t){
-            if(--m[c] < 0){
+
+        for (auto c : t) {
+            if (--m[c] < 0) {
                 return false;
             }
         }
-        
+
         return true;
     }
 };
 
-
-int main(){
-
-}
+int main() {}
