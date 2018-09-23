9a4e95c395091d71712d38fa283ebb7763045612
skywhat <i@skywhat.com>
2018-09-23 00:57:36 -0400

    update

--- 203.cpp
+++ 203.cpp
@@ -1,3 +1,10 @@
+// Remove all elements from a linked list of integers that have value val.
+//
+// Example:
+//
+// Input:  1->2->6->3->4->5->6, val = 6
+// Output: 1->2->3->4->5
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -12,16 +19,15 @@ public:
         ListNode prevHead(-1);
         prevHead.next = head;
         ListNode* prev = &prevHead;
-        
-        while(prev->next){
-            if(prev->next->val == val){
+
+        while (prev->next) {
+            if (prev->next->val == val) {
                 prev->next = prev->next->next;
-            }
-            else{
+            } else {
                 prev = prev->next;
             }
         }
-        
+
         return prevHead.next;
     }
 };
