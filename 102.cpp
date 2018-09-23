91e28657828f10475767101419a7431046510230
skywhat <i@skywhat.com>
2018-09-22 23:36:21 -0400

    update

--- 102.cpp
+++ 102.cpp
@@ -1,49 +1,53 @@
-#include<iostream>
-#include<vector>
 #include "Tree.h"
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-	vector<vector<int>> levelOrder(TreeNode* root){
-		queue<TreeNode*> q;
-		vector<vector<int>> res;
-		if(root)
-			q.push(root);
-		while(!q.empty()){
-			int size=q.size();
-			vector<int> row;
-			while(size--){
-				TreeNode* temp=q.front();
-				q.pop();
-				row.push_back(temp->val);
-				if(temp->left)
-					q.push(temp->left);
-				if(temp->right)
-					q.push(temp->right);
-			}
-			res.push_back(row);
-		}		
-		return res;
-	}
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        queue<TreeNode*> q;
+        vector<vector<int>> res;
+        if (root)
+            q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> row;
+
+            while (size--) {
+                TreeNode* temp = q.front();
+                q.pop();
+                row.push_back(temp->val);
+                if (temp->left)
+                    q.push(temp->left);
+                if (temp->right)
+                    q.push(temp->right);
+            }
+
+            res.push_back(row);
+        }
+
+        return res;
+    }
 };
 
-int main(){
-	TreeEnv t;
-	vector<int> list={3,9,20,NULL_TREENODE,NULL_TREENODE,15,7};
-	Solution s;
-	
-	TreeNode* root=t.initialTree(list);
-	vector<vector<int>> res=s.levelOrder(root);
-	for(auto row:res){
-		cout<<"[";
-		for(auto node:row)
-			cout<<node<<", ";
-		cout<<"]"<<endl;
-	}
-	cout<<endl;
+int main() {
+    TreeEnv t;
+    vector<int> list = {3, 9, 20, NULL_TREENODE, NULL_TREENODE, 15, 7};
+    Solution s;
 
-	return 0;
+    TreeNode* root = t.initialTree(list);
+    vector<vector<int>> res = s.levelOrder(root);
 
+    for (auto row : res) {
+        cout << "[";
+        for (auto node : row)
+            cout << node << ", ";
+        cout << "]" << endl;
+    }
+    cout << endl;
+
+    return 0;
 }
