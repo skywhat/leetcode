4d97489524db16141af12d1baa3a390c341a0ba5
skywhat <i@skywhat.com>
2018-09-23 22:00:26 -0400

    311. Sparse Matrix Multiplication

--- 311.cpp
+++ 311.cpp
@@ -1,27 +1,56 @@
-#include<iostream>
-#include<vector>
+// Given two sparse matrices A and B, return the result of AB.
+//
+// You may assume that A's column number is equal to B's row number.
+//
+// Example:
+//
+// Input:
+//
+// A = [
+//   [ 1, 0, 0],
+//   [-1, 0, 3]
+// ]
+//
+// B = [
+//   [ 7, 0, 0 ],
+//   [ 0, 0, 0 ],
+//   [ 0, 0, 1 ]
+// ]
+//
+// Output:
+//
+//      |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+// AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+//                   | 0 0 1 |
+
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
-        int m=A.size(),n=A[0].size(),nb=B[0].size();
-        vector<vector<int>> C(m,vector<int>(nb,0));
-        for(int i=0;i<m;++i){
-            for(int k=0;k<n;++k){
-                if(A[i][k]!=0){
-                    for(int j=0;j<nb;++j){
-                        if(B[k][j]!=0)
-                        C[i][j]+=A[i][k]*B[k][j];
-                    }
+    vector<vector<int>> multiply(vector<vector<int>>& A,
+                                 vector<vector<int>>& B) {
+        int m = A.size();
+        int mn = A[0].size();
+        int n = B[0].size();
+
+        vector<vector<int>> res(m, vector<int>(n, 0));
+
+        for (int i = 0; i < m; ++i) {
+            for (int k = 0; k < mn; ++k) {
+                if (A[i][k] == 0) {
+                    continue;
+                }
+                for (int j = 0; j < n; ++j) {
+                    res[i][j] += A[i][k] * B[k][j];
                 }
             }
         }
-        return C;
+
+        return res;
     }
 };
 
-int main(){
-
-}
+int main() {}
