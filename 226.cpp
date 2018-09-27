f63d5b7061685e392fa11052c02d047f77fcd406
skywhat <i@skywhat.com>
2018-09-27 11:47:52 -0400

    226. Invert Binary Tree

--- 226.cpp
+++ 226.cpp
@@ -1,45 +1,44 @@
-#include<iostream>
 #include "Tree.h"
+#include <iostream>
 
 using namespace std;
 
 class Solution {
 public:
-	TreeNode* invertTree(TreeNode* root) {
-		if (!root)
-			return nullptr;
-		if (!root->left && !root->right)
-			return root;
-		TreeNode* left = invertTree(root->left);
-		TreeNode* right = invertTree(root->right);
-		root->left = right;
-		root->right = left;
-		return root;
-	}
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+
+        TreeNode* right = invertTree(root->left);
+        TreeNode* left = invertTree(root->right);
+        root->left = left;
+        root->right = right;
+        return root;
+    }
 };
 
 class Solution2 {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if(root){
+        if (root) {
             invertTree(root->left);
             invertTree(root->right);
             swap(root->left, root->right);
         }
-        
+
         return root;
     }
 };
 
 int main() {
-	TreeEnv t;
-	vector<int> list = {4,2,7,1,3,6,9};
-	TreeNode* root=t.initialTree(list);
-	t.displayTree(root);
+    TreeEnv t;
+    vector<int> list = {4, 2, 7, 1, 3, 6, 9};
+    TreeNode* root = t.initialTree(list);
+    t.displayTree(root);
 
-	Solution s;
-	TreeNode* invertTree = s.invertTree(root);
-	t.displayTree(invertTree);
-	return 0;
+    Solution s;
+    TreeNode* invertTree = s.invertTree(root);
+    t.displayTree(invertTree);
+    return 0;
 }
-
