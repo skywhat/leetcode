fbf119328c94c8e74419b4b8f53460f44320807b
skywhat <i@skywhat.com>
2018-10-01 15:02:00 -0400

    572. Subtree of Another Tree

--- 572.cpp
+++ 572.cpp
@@ -1,30 +1,67 @@
-#include<iostream>
-#include "Tree.h"
+// Given two non-empty binary trees s and t, check whether tree t has exactly
+// the same structure and node values with a subtree of s. A subtree of s is a
+// tree consists of a node in s and all of this node's descendants. The tree s
+// could also be considered as a subtree of itself.
+//
+// Example 1:
+// Given tree s:
+//
+//      3
+//     / \
+//    4   5
+//   / \
+//  1   2
+// Given tree t:
+//    4
+//   / \
+//  1   2
+// Return true, because t has the same structure and node values with a subtree
+// of s. Example 2: Given tree s:
+//
+//      3
+//     / \
+//    4   5
+//   / \
+//  1   2
+//     /
+//    0
+// Given tree t:
+//    4
+//   / \
+//  1   2
+// Return false.
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
     bool isSubtree(TreeNode* s, TreeNode* t) {
-        if(!s){
+        if (s == nullptr) {
             return false;
         }
-        
-        return isSameTree(s, t) || isSubtree(s->left, t) || isSubtree(s->right, t); 
+        return isSameTree(s, t) || isSubtree(s->left, t)
+               || isSubtree(s->right, t);
     }
-    
-    bool isSameTree(TreeNode* s, TreeNode* t){
-        if(!s && !t){
+
+    bool isSameTree(TreeNode* s, TreeNode* t) {
+        if (s == nullptr && t == nullptr) {
             return true;
-        }
-        
-        if(!s || !t){
+        } else if (s == nullptr || t == nullptr) {
             return false;
         }
-        
-        if(s->val != t->val){
+
+        if (s->val != t->val) {
             return false;
         }
-        
-        return isSameTree(s->left, t->left) && 
-            isSameTree(s->right, t->right);
+
+        return isSameTree(s->left, t->left) && isSameTree(s->right, t->right);
+        ;
     }
 };
