278971d96e018f2e9108e9a6853cefe917a44709
skywhat <i@skywhat.com>
2018-10-01 20:20:08 -0400

    74. Search a 2D Matrix

--- 74.cpp
+++ 74.cpp
@@ -1,27 +1,56 @@
-#include<iostream>
-#include<vector>
+// Write an efficient algorithm that searches for a value in an m x n matrix.
+// This matrix has the following properties:
+//
+// Integers in each row are sorted from left to right.
+// The first integer of each row is greater than the last integer of the
+// previous row. Example 1:
+//
+// Input:
+// matrix = [
+//   [1,   3,  5,  7],
+//   [10, 11, 16, 20],
+//   [23, 30, 34, 50]
+// ]
+// target = 3
+// Output: true
+// Example 2:
+//
+// Input:
+// matrix = [
+//   [1,   3,  5,  7],
+//   [10, 11, 16, 20],
+//   [23, 30, 34, 50]
+// ]
+// target = 13
+// Output: false
+
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m=matrix.size();
-        if(!m)
+        int m = matrix.size();
+        if (!m) {
             return false;
-        int n=matrix[0].size();
-        int size=m*n;
-        int left=0,right=size-1;
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            int val=matrix[mid/n][mid%n];
-            if(val==target)
-                return true;
-            else if(val>target)
-                right=mid-1;
-            else
-                left=mid+1;
         }
+        int n = matrix[0].size();
+
+        int size = m * n;
+        int i = 0, j = size - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (matrix[mid / n][mid % n] == target) {
+                return true;
+            } else if (matrix[mid / n][mid % n] > target) {
+                j = mid - 1;
+            } else {
+                i = mid + 1;
+            }
+        }
+
         return false;
     }
 };
@@ -32,6 +61,4 @@ public:
 [10/4][10%4]
 */
 
-int main(){
-
-}
+int main() {}
