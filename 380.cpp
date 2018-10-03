e4e497fdc867a3f5369970ac4e6ebaf7dd19a311
skywhat <i@skywhat.com>
2018-10-03 02:10:37 -0400

    update

--- 380.cpp
+++ 380.cpp
@@ -1,42 +1,78 @@
-#include<iostream>
-#include<vector>
-#include<unordered_map>
+// Design a data structure that supports all following operations in average
+// O(1) time.
+//
+// insert(val): Inserts an item val to the set if not already present.
+// remove(val): Removes an item val from the set if present.
+// getRandom: Returns a random element from current set of elements. Each
+// element must have the same probability of being returned. Example:
+//
+// // Init an empty set.
+// RandomizedSet randomSet = new RandomizedSet();
+//
+// // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+// randomSet.insert(1);
+//
+// // Returns false as 2 does not exist in the set.
+// randomSet.remove(2);
+//
+// // Inserts 2 to the set, returns true. Set now contains [1,2].
+// randomSet.insert(2);
+//
+// // getRandom should return either 1 or 2 randomly.
+// randomSet.getRandom();
+//
+// // Removes 1 from the set, returns true. Set now contains [2].
+// randomSet.remove(1);
+//
+// // 2 was already in the set, so return false.
+// randomSet.insert(2);
+//
+// // Since 2 is the only number in the set, getRandom always return 2.
+// randomSet.getRandom();
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 class RandomizedSet {
 public:
-    unordered_map<int,int> m;
-    vector<int> help;
     /** Initialize your data structure here. */
-    RandomizedSet() {
-        
-    }
-    
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    RandomizedSet() {}
+    vector<int> nums;
+    unordered_map<int, int> m; // key:val value: index
+    /** Inserts a value to the set. Returns true if the set did not already
+     * contain the specified element. */
     bool insert(int val) {
-        if(m.find(val)!=m.end())
+        if (m.count(val)) {
             return false;
-        help.push_back(val);
-        m[val]=help.size()-1;
+        }
+
+        nums.push_back(val);
+        m[val] = nums.size() - 1;
         return true;
     }
-    
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
+
+    /** Removes a value from the set. Returns true if the set contained the
+     * specified element. */
     bool remove(int val) {
-        if(m.find(val)==m.end())
+        if (!m.count(val)) {
             return false;
-        int pos=m[val];
-        help[pos]=help.back();
-        help.pop_back();
-        m[help[pos]]=pos;
+        }
+
+        nums[m[val]] = nums.back();
+        m[nums.back()] = m[val];
+        nums.pop_back();
         m.erase(val);
+
         return true;
     }
-    
+
     /** Get a random element from the set. */
     int getRandom() {
-        return help[rand()%help.size()];
+        int ind = random() % nums.size();
+        return nums[ind];
     }
 };
 
@@ -48,6 +84,4 @@ public:
  * int param_3 = obj.getRandom();
  */
 
-int main(){
-
-}
+int main() {}
