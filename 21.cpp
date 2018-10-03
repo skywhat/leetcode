2e699539122d13c18c50fea246f1b1505affe9d5
skywhat <i@skywhat.com>
2018-10-03 02:13:43 -0400

    21. Merge Two Sorted Lists

--- 21.cpp
+++ 21.cpp
@@ -1,5 +1,13 @@
-#include<iostream>
-#include<vector>
+// Merge two sorted linked lists and return it as a new list. The new list
+// should be made by splicing together the nodes of the first two lists.
+//
+// Example:
+//
+// Input: 1->2->4, 1->3->4
+// Output: 1->1->2->3->4->4
+
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -8,41 +16,39 @@ public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         ListNode prevHead(-1);
         ListNode* prev = &prevHead;
-        
-        while(l1 && l2){
-            if(l1->val < l2->val){
+
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
                 prev->next = l1;
                 l1 = l1->next;
-            }
-            else{
+            } else {
                 prev->next = l2;
                 l2 = l2->next;
             }
             prev = prev->next;
         }
-        
-        prev->next = l1?l1:l2;
+
+        prev->next = l1 ? l1 : l2;
         return prevHead.next;
     }
 };
 
-
-//This solution is not a tail-recursive, the stack will overflow while the list is too long
+// This solution is not a tail-recursive, the stack will overflow while the list
+// is too long
 class Solution2 {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(!l1){
+        if (!l1) {
             return l2;
         }
-        if(!l2){
+        if (!l2) {
             return l1;
         }
-        
-        if(l1->val < l2->val){
+
+        if (l1->val < l2->val) {
             l1->next = mergeTwoLists(l1->next, l2);
             return l1;
-        }
-        else{
+        } else {
             l2->next = mergeTwoLists(l1, l2->next);
             return l2;
         }
