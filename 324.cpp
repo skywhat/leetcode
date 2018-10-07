981188ebad708902e7bb84a985cc25ad43ca98cf
skywhat <i@skywhat.com>
2018-10-07 13:12:05 -0400

    Wiggle Sort II

--- 324.cpp
+++ 324.cpp
@@ -1,16 +1,33 @@
-#include<iostream>
-#include<vector>
+// Given an unsorted array nums, reorder it such that nums[0] < nums[1] >
+// nums[2] < nums[3]....
+//
+// Example 1:
+//
+// Input: nums = [1, 5, 1, 1, 6, 4]
+// Output: One possible answer is [1, 4, 1, 5, 1, 6].
+// Example 2:
+//
+// Input: nums = [1, 3, 2, 2, 3, 1]
+// Output: One possible answer is [2, 3, 1, 3, 1, 2].
+// Note:
+// You may assume all input has valid answer.
+//
+// Follow Up:
+// Can you do it in O(n) time and/or in-place with O(1) extra space?
+
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
         vector<int> help(nums);
-        sort(help.begin(),help.end());
-        for(int i=0,k=(n-1)/2,l=n-1;i<n;++i){
-            nums[i]=help[i&1?l--:k--];
+        sort(help.begin(), help.end());
+        for (int i = 0, k = (n - 1) / 2, l = n - 1; i < n; ++i) {
+            nums[i] = help[i & 1 ? l-- : k--];
         }
     }
 };
@@ -30,5 +47,12 @@ even:
 
 3 7 2 6 1 5 0 4
 
+a b c d
+=>
+1. a c b d  wrong
+2. a d b c  wrong
+3. b c a d  wrong
+4. b d a c  right
 
+ensure consecutive numbers won't be adjacent after wiggling
 */
