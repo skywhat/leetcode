3edda043a23c3d79aec4e8a7c37091e789d0863c
skywhat <i@skywhat.com>
2018-10-16 13:57:16 -0400

    update

--- 341.cpp
+++ 341.cpp
@@ -1,51 +1,69 @@
-#include<iostream>
-#include<vector>
-#include<stack>
+// Given a nested list of integers, implement an iterator to flatten it.
+//
+// Each element is either an integer, or a list -- whose elements may also be
+// integers or other lists.
+//
+// Example 1:
+//
+// Input: [[1,1],2,[1,1]]
+// Output: [1,1,2,1,1]
+// Explanation: By calling next repeatedly until hasNext returns false,
+//              the order of elements returned by next should be: [1,1,2,1,1].
+// Example 2:
+//
+// Input: [1,[4,[6]]]
+// Output: [1,4,6]
+// Explanation: By calling next repeatedly until hasNext returns false,
+//              the order of elements returned by next should be: [1,4,6].
 
-using namespace std;
 /**
  * // This is the interface that allows for creating nested lists.
  * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than
+ * a nested list. bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a
+ * single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a
+ * nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
  */
- class NestedInteger {
-   public:
-     // Return true if this NestedInteger holds a single integer, rather than a nested list.
-     bool isInteger() const;
-
-     // Return the single integer that this NestedInteger holds, if it holds a single integer
-     // The result is undefined if this NestedInteger holds a nested list
-     int getInteger() const;
-
-     // Return the nested list that this NestedInteger holds, if it holds a nested list
-     // The result is undefined if this NestedInteger holds a single integer
-     const vector<NestedInteger> &getList() const;
- };
 class NestedIterator {
 public:
-    stack<NestedInteger> s;
-    NestedIterator(vector<NestedInteger> &nestedList) {
-        for(int i=nestedList.size()-1;i>=0;--i){
-            s.push(nestedList[i]);
+    stack<NestedInteger> st;
+    NestedIterator(vector<NestedInteger>& nestedList) {
+        for (int i = nestedList.size() - 1; i >= 0; --i) {
+            st.push(nestedList[i]);
         }
     }
-    
+
     int next() {
-        int result=s.top().getInteger();
-        s.pop();
-        return result;
+        auto res = st.top();
+        st.pop();
+        return res.getInteger();
     }
-    
+
     bool hasNext() {
-        while(!s.empty()){
-            NestedInteger tmp=s.top();
-            if(tmp.isInteger())
+        while (!st.empty()) {
+            auto temp = st.top();
+            if (temp.isInteger()) {
                 return true;
-            s.pop();
-            vector<NestedInteger>& next=tmp.getList();
-            for(int i=next.size()-1;i>=0;--i){
-                s.push(next[i]);
+            }
+
+            st.pop();
+            vector<NestedInteger>& nestedList = temp.getList();
+            for (int i = nestedList.size() - 1; i >= 0; --i) {
+                st.push(nestedList[i]);
             }
         }
+
         return false;
     }
 };
