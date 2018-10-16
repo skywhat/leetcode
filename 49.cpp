81af99c25a51b039227ae3371586cdfa7813426c
skywhat <i@skywhat.com>
2018-10-15 22:19:42 -0400

    49. Group Anagrams

--- 49.cpp
+++ 49.cpp
@@ -1,46 +1,46 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<unordered_map>
-#include<unordered_set>
-#include<algorithm>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-	vector<vector<string>> groupAnagrams(vector<string>& strs){
-		vector<string> temp(strs.begin(),strs.end());
-		for(auto& t:temp)
-			sort(t.begin(),t.end());
-		unordered_map<string,unordered_set<int>> m;
-		for(int i=0;i<temp.size();++i)
-			m[temp[i]].insert(i);
-		vector<vector<string>> res(m.size());
-		int row=0;
-		for(auto& item:m){
-			for(auto index:item.second)
-				res[row].push_back(strs[index]);
-			row++;
-		}
-		return res;
-	}
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string, vector<int>> m;
+        for (int i = 0; i < strs.size(); ++i) {
+            string s = strs[i];
+            sort(s.begin(), s.end());
+            m[s].push_back(i);
+        }
+
+        vector<vector<string>> res(m.size());
+        int row = 0;
+        for (auto item : m) {
+            for (auto i : item.second) {
+                res[row].push_back(strs[i]);
+            }
+            row++;
+        }
+
+        return res;
+    }
 };
 
-int main(){
-	vector<string> test={"eat", "tea", "tan", "ate", "nat", "bat"};
-	Solution s;
-	vector<vector<string>> res=s.groupAnagrams(test);
+int main() {
+    vector<string> test = {"eat", "tea", "tan", "ate", "nat", "bat"};
+    Solution s;
+    vector<vector<string>> res = s.groupAnagrams(test);
 
-	for(auto i:res){
-		cout<<"[";
-		for(auto j:i){
-			cout<<j<<", ";
-		}
-		cout<<"]"<<endl;
+    for (auto i : res) {
+        cout << "[";
+        for (auto j : i) {
+            cout << j << ", ";
+        }
+        cout << "]" << endl;
+    }
 
-	}
-
-	return 0;
-
+    return 0;
 }
-
