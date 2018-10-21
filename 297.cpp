cee11a3b72c623b887905a6820cb8ea4ecce51a4
skywhat <i@skywhat.com>
2018-10-21 12:35:51 -0400

    297. Serialize and Deserialize Binary Tree

--- 297.cpp
+++ 297.cpp
@@ -1,3 +1,31 @@
+// Serialization is the process of converting a data structure or object into a
+// sequence of bits so that it can be stored in a file or memory buffer, or
+// transmitted across a network connection link to be reconstructed later in the
+// same or another computer environment.
+//
+// Design an algorithm to serialize and deserialize a binary tree. There is no
+// restriction on how your serialization/deserialization algorithm should work.
+// You just need to ensure that a binary tree can be serialized to a string and
+// this string can be deserialized to the original tree structure.
+//
+// Example:
+//
+// You may serialize the following tree:
+//
+//     1
+//    / \
+//   2   3
+//      / \
+//     4   5
+//
+// as "[1,2,3,null,null,4,5]"
+// Clarification: The above format is the same as how LeetCode serializes a
+// binary tree. You do not necessarily need to follow this format, so please be
+// creative and come up with different approaches yourself.
+//
+// Note: Do not use class member/global/static variables to store states. Your
+// serialize and deserialize algorithms should be stateless.
+
 #include "Tree.h"
 #include <iostream>
 #include <queue>
@@ -18,8 +46,9 @@ public:
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) { return buildString(root); }
     string buildString(TreeNode* root) {
-        if (!root)
+        if (!root) {
             return "N#";
+        }
         string s = to_string(root->val) + "#";
         s += buildString(root->left);
         s += buildString(root->right);
@@ -31,20 +60,25 @@ public:
         queue<string> q;
         for (int i = 0; i < data.size();) {
             int cnt = 1;
-            while (data[i + cnt] != '#')
+            while (data[i + cnt] != '#') {
                 cnt++;
+            }
             q.push(data.substr(i, cnt));
             i += cnt + 1;
         }
         return buildTree(q);
     }
     TreeNode* buildTree(queue<string>& q) {
-        if (q.empty())
+        if (q.empty()) {
             return nullptr;
+        }
         string temp = q.front();
         q.pop();
-        if (temp == "N")
+
+        if (temp == "N") {
             return nullptr;
+        }
+
         TreeNode* root = new TreeNode(stoi(temp));
         root->left = buildTree(q);
         root->right = buildTree(q);
@@ -59,6 +93,7 @@ public:
         if (root == nullptr) {
             return "N ";
         }
+
         string res;
         res += to_string(root->val) + " ";
         res += serialize(root->left);
