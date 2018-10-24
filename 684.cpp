9e47c21e4a76c4ed8567cbddf56560b77e9fe438
skywhat <i@skywhat.com>
2018-10-24 09:31:05 -0700

    684. Redundant Connection

---
+++ 684.cpp
@@ -0,0 +1,66 @@
+// In this problem, a tree is an undirected graph that is connected and has no
+// cycles.
+//
+// The given input is a graph that started as a tree with N nodes (with distinct
+// values 1, 2, ..., N), with one additional edge added. The added edge has two
+// different vertices chosen from 1 to N, and was not an edge that already
+// existed.
+//
+// The resulting graph is given as a 2D-array of edges. Each element of edges is
+// a pair [u, v] with u < v, that represents an undirected edge connecting nodes
+// u and v.
+//
+// Return an edge that can be removed so that the resulting graph is a tree of N
+// nodes. If there are multiple answers, return the answer that occurs last in
+// the given 2D-array. The answer edge [u, v] should be in the same format, with
+// u < v.
+//
+// Example 1:
+// Input: [[1,2], [1,3], [2,3]]
+// Output: [2,3]
+// Explanation: The given undirected graph will be like this:
+//   1
+//  / \
+// 2 - 3
+// Example 2:
+// Input: [[1,2], [2,3], [3,4], [1,4], [1,5]]
+// Output: [1,4]
+// Explanation: The given undirected graph will be like this:
+// 5 - 1 - 2
+//     |   |
+//     4 - 3
+// Note:
+// The size of the input 2D-array will be between 3 and 1000.
+// Every integer represented in the 2D-array will be between 1 and N, where N is
+// the size of the input array.
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int> uf(n + 1);
+        for (int i = 0; i <= n; ++i) {
+            uf[i] = i;
+        }
+
+        for (auto e : edges) {
+            int r0 = find(uf, e[0]);
+            int r1 = find(uf, e[1]);
+            if (r0 != r1) {
+                uf[r0] = r1;
+            } else {
+                return e;
+            }
+        }
+
+        return {};
+    }
+
+    int find(vector<int>& uf, int id) {
+        while (uf[id] != id) {
+            uf[id] = uf[uf[id]]; // path compression
+            id = uf[id];
+        }
+        return id;
+    }
+};
