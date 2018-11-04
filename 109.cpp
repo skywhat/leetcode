9a63471ba092450d7ca1d7300a0b35c3acd5e4e8
skywhat <i@skywhat.com>
2018-11-04 18:27:48 -0500

    update

--- 109.cpp
+++ 109.cpp
@@ -1,33 +1,63 @@
-#include<iostream>
-#include<vector>
+// Given a singly linked list where elements are sorted in ascending order,
+// convert it to a height balanced BST.
+//
+// For this problem, a height-balanced binary tree is defined as a binary tree
+// in which the depth of the two subtrees of every node never differ by more
+// than 1.
+//
+// Example:
+//
+// Given the sorted linked list: [-10,-3,0,5,9],
+//
+// One possible answer is: [0,-3,9,-10,null,5], which represents the following
+// height balanced BST:
+//
+//       0
+//      / \
+//    -3   9
+//    /   /
+//  -10  5
 
-#include "Tree.h"
-#include "ListNode.h"
-
-class Solution{
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
 public:
-	TreeNode* sortedListToBST(ListNode* head){
-		return build(head,null);
-	}
-	TreeNode* build(ListNode* head,ListNode* tail){
-		if(head==tail)
-			return nullptr;
-		if(head->next==tail)
-			return head;
-		ListNode* slow;
-		ListNode* fast;
-		while(fast!=tail&&fast->next!=tail){
-			fast=fast->next->next;
-			slow=slow->next;
-		}
-		TreeNode* root=new TreeNode(slow->val);
-		root->left=build(head,slow);
-		root->right=build(slow->next,tail);
-		return root;
-	}
+    TreeNode* sortedListToBST(ListNode* head) { return build(head, nullptr); }
 
+    TreeNode* build(ListNode* head, ListNode* tail) {
+        if (head == tail) {
+            return nullptr;
+        }
+        if (head->next == tail) {
+            return new TreeNode(head->val);
+        }
+
+        ListNode *slow = head, *fast = head;
+
+        while (fast->next != tail && fast->next->next != tail) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        TreeNode* root = new TreeNode(slow->val);
+        root->left = build(head, slow);
+        root->right = build(slow->next, tail);
+
+        return root;
+    }
 };
-
-int main(){
-
-}
