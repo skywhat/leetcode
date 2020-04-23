f37019a84575dead0e86aef95baaf9f6a51055c9
Gang Zhang <gangzh@uber.com>
2020-04-23 00:43:31 -0700

    Count and say

--- Python/38.py
+++ Python/38.py
@@ -18,28 +18,27 @@ class Solution(object):
         res += str(cnt) + sayn_1[-1]
         return res
 
-
 class Solution(object):
     def countAndSay(self, n):
         """
         :type n: int
         :rtype: str
         """
-        prev = "1"
-        res = "1"
-        for i in range(2, n+1):
-            res = self.sayN(prev)
-            prev = res
-        return res
-
-    def sayN(self, prev):
-        cnt = 1
         res = ""
-        for i in range(1, len(prev)):
-            if prev[i] == prev[i-1]:
-                cnt += 1
-            else:
-                res += str(cnt) + prev[i-1]
-                cnt = 1
-        res += str(cnt) + prev[-1]
+        for i in range(1, n+1):
+            res = self.Say(res)
         return res
+    
+    def Say(self, s):
+        if s == "":
+            return "1"
+        res = ""
+        pos = 0
+        for i in range(1, len(s)):
+            if s[i] != s[pos]:
+                res += str(i-pos) + s[pos]
+                c = s[i]
+                pos = i
+        res += str(len(s)-pos) + s[-1]
+        return res
+        
