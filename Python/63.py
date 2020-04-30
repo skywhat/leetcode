8b204c9b4b307034bc8216b105114661244e22e9
Gang Zhang <gangzh@uber.com>
2020-04-30 15:22:51 -0700

    unique paths ||

---
+++ Python/63.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        m = len(obstacleGrid)
+        if m == 0:
+            return 0
+        n = len(obstacleGrid[0])
+        dp = [0]*n
+        for i in range(n):
+            if obstacleGrid[0][i] == 1:
+                break
+            dp[i] = 1
+        for i in range(1, m):
+            if obstacleGrid[i][0] == 1:
+                dp[0] = 0
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 1:
+                    dp[j] = 0
+                    continue
+                dp[j] += dp[j-1]
+        return dp[n-1]
