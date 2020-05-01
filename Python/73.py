2a39945d1a8faf68261632c4ca808e2e0f820ebe
Gang Zhang <gangzh@uber.com>
2020-04-30 21:01:16 -0700

    set matrix zeroes

---
+++ Python/73.py
@@ -0,0 +1,56 @@
+#O(m+n) space complexity
+
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        rows = set()
+        cols = set()
+        m, n = len(matrix), len(matrix[0])
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    rows.add(i)
+                    cols.add(j)
+        for i in range(m):
+            for j in range(n):
+                if i in rows or j in cols:
+                    matrix[i][j] = 0
+
+
+
+#O(1) space complexity
+
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        first_row_zero = first_col_zero = False
+        m, n = len(matrix), len(matrix[0])
+        for i in range(m):
+            if matrix[i][0] == 0:
+                first_col_zero = True
+                break
+        for j in range(n):
+            if matrix[0][j] == 0:
+                first_row_zero = True
+                break
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = matrix[i][0] = 0
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        if first_row_zero:
+            for j in range(n):
+                matrix[0][j] = 0
+        if first_col_zero:
+            for i in range(m):
+                matrix[i][0] = 0
