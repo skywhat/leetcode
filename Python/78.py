ae60d258eed0c7d122cd470b4b1fcded3c983e1e
Gang Zhang <gangzh@uber.com>
2020-05-01 21:27:44 -0700

    subsets

--- Python/78.py
+++ Python/78.py
@@ -15,3 +15,16 @@ class Solution(object):
         self.pick(nums, pos+1, res, l)
         self.pick(nums, pos+1, res, l+[nums[pos]])
         
+
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = [[]]
+        for i in range(len(nums)):
+            n = len(res)
+            for j in range(n):
+                res.append(res[j]+[nums[i]])
+        return res
