477367f82d56e8e998b2c42ed178fdfcf3e7bde7
Gang Zhang <gangzh@uber.com>
2020-05-02 23:58:41 -0700

    valid palindrome

---
+++ Python/125.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        i, j = 0, len(s) -1
+        while i<j:
+            while i<j and not s[i].isalnum():
+                i+=1
+            while i<j and not s[j].isalnum():
+                j-=1
+            if i<j and s[i].lower() != s[j].lower():
+                return False
+            i+=1
+            j-=1
+        return True
