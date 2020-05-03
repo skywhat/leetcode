e6945003602b5520dfa68259b63025753c7f9144
Gang Zhang <gangzh@uber.com>
2020-05-03 00:01:04 -0700

    single number

---
+++ Python/136.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        for n in nums:
+            res ^=n
+        return res
