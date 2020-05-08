ac75a46bbcc4840390594f22ae0c02f19c5b3b02
Gang Zhang <gangzh@uber.com>
2020-05-07 22:39:20 -0700

    binary tree inorder traversal

---
+++ Python/94.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        self.dfs(root, res)
+        return res
+    
+    def dfs(self, root, res):
+        if root:
+            self.dfs(root.left, res)
+            res.append(root.val)
+            self.dfs(root.right, res)
+
+
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        stack = []
+
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            res.append(root.val)
+            root = root.right
+        return res
