9d956999cddaf4e937c3933fb738ef061438447a
Gang Zhang <gangzh@uber.com>
2020-05-16 22:55:04 -0700

    interleaving string

---
+++ Python/97.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isInterleave(self, s1, s2, s3):
+        """
+        :type s1: str
+        :type s2: str
+        :type s3: str
+        :rtype: bool
+        """
+        m, n = len(s1), len(s2)
+        if m+n != len(s3):
+            return False
+        dp = [[False]*(n+1) for i in range(m+1)]
+        dp[0][0] = True
+        for i in range(1, m+1):
+            dp[i][0] = dp[i-1][0] and s1[i-1] == s3[i-1]
+        for j in range(1, n+1):
+            dp[0][j] = dp[0][j-1] and s2[j-1] == s3[j-1]
+            
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i+j-1]) \
+                or (dp[i][j-1] and s2[j-1] == s3[i+j-1])
+        return dp[m][n]
