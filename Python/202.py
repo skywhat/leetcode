26e0834d7de462b3726d7c09469a085af73c4d11
Gang Zhang <gangzh@uber.com>
2020-05-26 19:31:32 -0700

    happy number

--- Python/202.py
+++ Python/202.py
@@ -4,18 +4,10 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        existingNums = []
-        while True:
-	        if n in existingNums:
-		        return False
-	        elif n == 1:
-		        return True
-	        existingNums.append(n)
-	        n = self.sumSquare(n)
-            
-    def sumSquare(self, n):
-	    res = 0
-	    while n!=0:
-		    res += (n%10)**2
-		    n /=10
-	    return res
+        seen = set()
+        while n not in seen:
+            seen.add(n)
+            n = sum([int(i)**2 for i in str(n)])
+            if n == 1:
+                return True
+        return False
