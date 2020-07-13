03ac7254f92d28766426217a4d9edb13e7b4d44f
Gang Zhang <gangzh@uber.com>
2020-07-12 18:06:48 -0700

    next permutation

--- Python/31.py
+++ Python/31.py
@@ -22,3 +22,31 @@ class Solution(object):
             nums[i], nums[j] = nums[j], nums[i]
             i+=1
             j-=1
+
+
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        # from right to left, find the first one decreasing.
+        K = -1
+        for i in range(len(nums)-2, -1, -1):
+            if nums[i] < nums[i+1]:
+                K = i
+                break
+        if K == -1:
+            nums.reverse()
+            return
+        J = -1
+        for i in range(len(nums)-1, K, -1):
+            if nums[i] > nums[K]:
+                J = i
+                break
+        nums[K], nums[J] = nums[J], nums[K]
+        i, j = K+1, len(nums)-1
+        while i<j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i+=1
+            j-=1
