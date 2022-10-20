a44f6ec031faf53cd111c44f33459364cc93abdc
skywhat <gangzh@uber.com>
2022-10-19 23:50:27 -0700

    Create 217.py

---
+++ Python/217.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        s = set()
+        for n in nums:
+            if n in s:
+                return True
+            else:
+                s.add(n)            
+        return False
+      
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        s = set(nums)
+        return len(nums) > len(s)
