78a51e75d1d6b63934d09f5107b8b2ba1894f672
skywhat <gangzh@uber.com>
2022-11-09 00:27:17 -0800

    Update 17.py

--- Python/17.py
+++ Python/17.py
@@ -12,3 +12,23 @@ class Solution(object):
             return list(self.mapping[digits[0]])
         prev = self.letterCombinations(digits[:-1])
         return [s + c for s in prev for c in self.mapping[digits[-1]]]
+
+class Solution(object):
+    mapping = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        res = []
+        if not digits:
+            return []
+        self.dfs(digits, "", 0, res)
+        return res
+
+    def dfs(self, digits, path, pos, res):
+        if pos >= len(digits):
+            res.append(path)
+            return
+        for c in self.mapping[digits[pos]]:
+            self.dfs(digits, path+c, pos+1, res)
