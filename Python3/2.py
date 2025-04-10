7ef9bd726bccf4d3c23a718ad22c9ab9fbaa02ca
skywhat <i@skywhat.com>
2025-04-10 08:59:09 -0700

    add two numbers

---
+++ Python3/2.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        prevHead = prev = ListNode(-1)
+        carry = 0
+        while l1 or l2 or carry:
+            val = carry
+            if l1:
+                val += l1.val
+                l1 = l1.next
+            if l2:
+                val += l2.val
+                l2 = l2.next
+            carry = val // 10
+            prev.next = ListNode(val % 10)
+            prev = prev.next
+        return prevHead.next
