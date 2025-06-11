dd1c8d09646595db502e3862e37b07a9e1340bb9
skywhat <i@skywhat.com>
2025-06-10 21:12:11 -0700

    Number of Islands II

--- Python3/305.py
+++ Python3/305.py
@@ -1,27 +1,33 @@
 class Solution:
     def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
-        uf = [i for i in range(m*n)]
+        uf = list(range(m*n))
         grid = [[0]*n for _ in range(m)]
 
-        res = []
         cnt = 0
-        for x, y in positions:
-            cur = x*n + y
-            if grid[x][y]:
+        res = []
+        for i, j in positions:
+            if grid[i][j]:
                 res.append(cnt)
                 continue
-            cnt+=1
-            grid[x][y] = 1
-            for p, q in [[x-1,y],[x+1,y],[x,y-1],[x,y+1]]:
-                if p<0 or p>=m or q<0 or q>=n or grid[p][q] == 0:
+            cur = i*n+j
+            cur_root = self.find(uf, cur)
+            cnt +=1
+            grid[i][j] = 1
+            for x, y in [[i-1,j],[i+1,j],[i,j-1],[i,j+1]]:
+                if x<0 or x>=m or y<0 or y>=n or grid[x][y] == 0:
                     continue
-                nb = self.find(uf, p*n+q)
-                cur_root = self.find(uf, cur)
-                if nb != cur_root:
-                    uf[cur_root] = nb
+                ne = x*n+y
+                ne_root = self.find(uf, ne)
+                if cur_root != ne_root:
+                    # current position is the central position to connect nearby islands
+                    # so use cur_root as the root of the nearby island.
+                    uf[ne_root] = cur_root
                     cnt -=1
+            
             res.append(cnt)
         return res
+
+    
 
     def find(self, uf, p):
         if uf[p] != p:
