6e693826c6c154e02a942d01d0cf1c7ef275c596
skywhat <i@skywhat.com>
2025-06-11 20:40:39 -0700

    meeting rooms II

--- Python3/253.py
+++ Python3/253.py
@@ -11,3 +11,17 @@ class Solution:
                     heapq.heappop(h)
                 heapq.heappush(h, end)
         return len(h)
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        count = collections.defaultdict(int)
+        for i, j in intervals:
+            count[i] +=1
+            count[j] -=1
+        
+        res = 0
+        room = 0
+        for t in sorted(count.keys()):
+            room += count[t]
+            res = max(res, room)
+        return res
