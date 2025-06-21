7d33b1785863ac791fbd8f102a70b472e24c748f
skywhat <i@skywhat.com>
2025-06-20 20:54:33 -0700

    contains duplicate

---
+++ Python3/217.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for n in nums:
+            if n in s:
+                return True
+            s.add(n)
+        return False
