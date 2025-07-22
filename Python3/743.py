e8da34f96f1e3da54f5c3b5a1ba6c90d439042a3
skywhat <i@skywhat.com>
2025-07-22 15:12:54 -0700

    network delay time

--- Python3/743.py
+++ Python3/743.py
@@ -15,3 +15,28 @@ class Solution:
             for next_node in graph[node]:
                 heapq.heappush(h, (graph[node][next_node] + t, next_node))
         return res if len(visited) == n else -1
+
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = collections.defaultdict(dict)
+        all = set()
+        for a, b, t in times:
+            graph[a][b] = t
+            all.add(a)
+            all.add(b)
+        if len(all) != n:
+            return -1
+        
+        h = [(0, k)]
+        visited = set()
+        while h:
+            cur_time, node = heapq.heappop(h)
+            if node in visited:
+                continue
+            visited.add(node)
+            for next_node, t in graph[node].items():
+                heapq.heappush(h, (cur_time+t, next_node))
+            if len(visited) == n:
+                return cur_time
+        return -1
