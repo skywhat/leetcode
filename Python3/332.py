0addc98e778ddf719fa46547beac3523ede68459
skywhat <i@skywhat.com>
2025-07-22 17:04:18 -0700

    itinerary update comment

--- Python3/332.py
+++ Python3/332.py
@@ -22,4 +22,16 @@ end node is start node iff all nodes has even degree.
 end node is another node iff there is another odd degree node and start node has an odd degree.
 So, the algorithm is to find the end node first and delete the path to this node(backtrack)
 
+
+Example:
+JFK: A, D
+A: C
+B: C
+C: D, JFK
+D: A, B
+
+
+From JFK we first visit JFK -> A -> C -> D -> A. There we're stuck, so we write down A as the end of the route and retreat back to D. There we see the unused ticket to B and follow it: D -> B -> C -> JFK -> D. Then we're stuck again, retreat and write down the airports while doing so: Write down D before the already written A, then JFK before the D, etc. When we're back from our cycle at D, the written route is D -> B -> C -> JFK -> D -> A. Then we retreat further along the original path, prepending C, A and finally JFK to the route, ending up with the route JFK -> A -> C -> D -> B -> C -> JFK -> D -> A.
+
+
 """
